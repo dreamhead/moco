01be1ecaf90c2945a6f9bed05ab883522eaa8fe6
dreamhead <dreamhead.cn@gmail.com>
2012-11-19 21:06:20 +1000

    upgraded junit to 4.11

--- moco-runner/src/main/java/com/github/dreamhead/moco/runner/StandaloneRunner.java
+++ moco-runner/src/main/java/com/github/dreamhead/moco/runner/StandaloneRunner.java
@@ -2,12 +2,18 @@ package com.github.dreamhead.moco.runner;
 
 import com.github.dreamhead.moco.HttpServer;
 import com.github.dreamhead.moco.internal.MocoHttpServer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class StandaloneRunner {
+    private static Logger logger = LoggerFactory.getLogger(StandaloneRunner.class);
+
     private MocoHttpServer server;
 
     public void run(HttpServer httpServer) {
         server = new MocoHttpServer(httpServer);
+
+        logger.info("Server is started at {}", httpServer.getPort());
 
         Runtime.getRuntime().addShutdownHook(new Thread() {
             @Override
