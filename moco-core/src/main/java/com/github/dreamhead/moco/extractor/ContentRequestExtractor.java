470add2a0647798b67c99bc86079c18bfe6fb7c7
dreamhead <dreamhead.cn@gmail.com>
2012-11-20 22:08:58 +1000

    used netty content reader

--- moco-core/src/main/java/com/github/dreamhead/moco/extractor/ContentRequestExtractor.java
+++ moco-core/src/main/java/com/github/dreamhead/moco/extractor/ContentRequestExtractor.java
@@ -1,8 +1,6 @@
 package com.github.dreamhead.moco.extractor;
 
 import com.github.dreamhead.moco.RequestExtractor;
-import org.jboss.netty.buffer.ChannelBuffer;
-import org.jboss.netty.handler.codec.http.HttpHeaders;
 import org.jboss.netty.handler.codec.http.HttpRequest;
 
 import java.nio.charset.Charset;
@@ -10,18 +8,6 @@ import java.nio.charset.Charset;
 public class ContentRequestExtractor implements RequestExtractor {
     @Override
     public String extract(HttpRequest request) {
-        return requestContent(request);
-    }
-
-    private String requestContent(HttpRequest request) {
-        return retrieveContent(request.getContent(), HttpHeaders.getContentLength(request));
-    }
-
-    private String retrieveContent(ChannelBuffer contentBuffer, long contentLength) {
-        return contentBuffer.toString(contentStartIndex(contentLength, contentBuffer), (int) contentLength, Charset.defaultCharset());
-    }
-
-    private int contentStartIndex(long contentLength, ChannelBuffer contentBuffer) {
-        return (int) (contentBuffer.capacity() - contentLength);
+        return request.getContent().toString(Charset.defaultCharset());
     }
 }
