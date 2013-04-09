febfd39df1c810fc7207a9c485399290ec89f142
dreamhead <dreamhead.cn@gmail.com>
2013-04-09 17:08:52 +0800

    used netty client to shutdown

--- moco-runner/src/main/java/com/github/dreamhead/moco/bootstrap/ShutdownTask.java
+++ moco-runner/src/main/java/com/github/dreamhead/moco/bootstrap/ShutdownTask.java
@@ -1,21 +1,21 @@
 package com.github.dreamhead.moco.bootstrap;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.jboss.netty.bootstrap.ClientBootstrap;
+import org.jboss.netty.buffer.ChannelBuffer;
+import org.jboss.netty.channel.*;
+import org.jboss.netty.channel.socket.nio.NioClientSocketChannelFactory;
 
-import java.io.IOException;
-import java.io.OutputStream;
 import java.net.ConnectException;
-import java.net.InetAddress;
-import java.net.Socket;
+import java.net.InetSocketAddress;
+import java.util.concurrent.Executors;
 
 import static com.github.dreamhead.moco.bootstrap.ShutdownArgs.parse;
+import static org.jboss.netty.buffer.ChannelBuffers.buffer;
 
 public class ShutdownTask implements BootstrapTask {
-    private static Logger logger = LoggerFactory.getLogger(ShutdownTask.class);
-
     private final int defaultShutdownPort;
     private final String defaultShutdownKey;
+    private MocoClient client = new MocoClient();
 
     public ShutdownTask(int defaultShutdownPort, String defaultShutdownKey) {
         this.defaultShutdownPort = defaultShutdownPort;
@@ -25,22 +25,66 @@ public class ShutdownTask implements BootstrapTask {
     @Override
     public void run(String[] args) {
         ShutdownArgs shutdownArgs = parse(args);
-        socketShutdown(shutdownArgs.getShutdownPort(defaultShutdownPort), defaultShutdownKey);
+        client.run(shutdownArgs.getShutdownPort(defaultShutdownPort), new ChannelPipelineFactory() {
+            public ChannelPipeline getPipeline() {
+                return Channels.pipeline(new ShutdownHandler());
+            }
+        });
     }
 
-    private void socketShutdown(int shutdownPort, String shutdownMocoKey) {
-        try {
-            Socket socket = new Socket(InetAddress.getByName("127.0.0.1"), shutdownPort);
-            socket.setSoLinger(false, 0);
-            OutputStream outputStream = socket.getOutputStream();
-            outputStream.write((shutdownMocoKey + "\r\n").getBytes());
-            outputStream.flush();
-            socket.close();
-        } catch (ConnectException e) {
-            System.err.println("fail to shutdown, please specify correct shutdown port.");
-        } catch (IOException e) {
-            logger.error("exception is thrown", e);
-            throw new RuntimeException(e);
+    private static class MocoClient {
+        private ClientBootstrap bootstrap;
+
+        public void run(final int port, final ChannelPipelineFactory pipelineFactory) {
+            ChannelFactory factory = new NioClientSocketChannelFactory(
+                    Executors.newCachedThreadPool(),
+                    Executors.newCachedThreadPool());
+
+            bootstrap = new ClientBootstrap(factory);
+            bootstrap.setPipelineFactory(pipelineFactory);
+
+            bootstrap.setOption("tcpNoDelay", true);
+            bootstrap.setOption("keepAlive", true);
+
+            bootstrap.connect(new InetSocketAddress("127.0.0.1", port));
+        }
+
+        public void stop() {
+            if (bootstrap != null) {
+                bootstrap.releaseExternalResources();
+                bootstrap = null;
+            }
+        }
+    }
+
+    private class ShutdownHandler extends SimpleChannelHandler {
+        @Override
+        public void channelConnected(ChannelHandlerContext ctx, ChannelStateEvent e) throws Exception {
+            byte[] bytes = (defaultShutdownKey + "\r\n").getBytes();
+            ChannelBuffer buf = buffer(bytes.length);
+            buf.writeBytes(bytes);
+            Channels.write(ctx, e.getFuture(), buf);
+        }
+
+        @Override
+        public void channelClosed(ChannelHandlerContext ctx, ChannelStateEvent e) throws Exception {
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    client.stop();
+                }
+            }).start();
+        }
+
+        @Override
+        public void exceptionCaught(ChannelHandlerContext ctx, ExceptionEvent e) throws Exception {
+            Throwable cause = e.getCause();
+            if (ConnectException.class.isInstance(cause)) {
+                System.err.println("fail to shutdown, please specify correct shutdown port.");
+                return;
+            }
+
+            throw new RuntimeException(cause);
         }
     }
 }
