aee3b743ed84865193beda34bfc4bb48bb20a470
dreamhead <dreamhead.cn@gmail.com>
2013-04-22 05:42:28 +0800

    added favicon in runner

--- moco-runner/src/main/java/com/github/dreamhead/moco/runner/JsonRunner.java
+++ moco-runner/src/main/java/com/github/dreamhead/moco/runner/JsonRunner.java
@@ -5,13 +5,15 @@ import com.github.dreamhead.moco.parser.HttpServerParser;
 
 import java.io.InputStream;
 
+import static com.github.dreamhead.moco.Moco.*;
+
 public class JsonRunner {
 
     private final HttpServerParser httpServerParser = new HttpServerParser();
     private final StandaloneRunner runner = new StandaloneRunner();
 
     public void run(InputStream is, int port) {
-        runner.run(httpServerParser.parseServer(is, port));
+        run(createServer(is, port));
     }
 
     public void stop() {
@@ -19,12 +21,18 @@ public class JsonRunner {
     }
 
     public void restart(InputStream is, int port) {
-        HttpServer httpServer = httpServerParser.parseServer(is, port);
+        HttpServer httpServer = createServer(is, port);
         stop();
         run(httpServer);
     }
 
     private void run(HttpServer httpServer) {
         runner.run(httpServer);
+    }
+
+    private HttpServer createServer(InputStream is, int port) {
+        HttpServer httpServer = httpServerParser.parseServer(is, port);
+        httpServer.request(by(uri("/favicon.ico"))).response(content(pathResource("favicon.png")), header("Content-Type", "image/png"));
+        return httpServer;
     }
 }
