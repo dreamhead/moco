6ab690122270bc8c1fb390247c0786bf28162ab7
dreamhead <dreamhead.cn@gmail.com>
2013-06-20 08:58:27 +0800

    added proxy test for stanalone mode

---
+++ moco-runner/src/test/java/com/github/dreamhead/moco/MocoProxyStandaloneTest.java
@@ -0,0 +1,18 @@
+package com.github.dreamhead.moco;
+
+import org.junit.Test;
+
+import java.io.IOException;
+
+import static com.github.dreamhead.moco.RemoteTestUtils.remoteUrl;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+public class MocoProxyStandaloneTest extends AbstractMocoStandaloneTest {
+    @Test
+    public void should_response_with_proxy() throws IOException {
+        runWithConfiguration("proxy.json");
+        String content = helper.get(remoteUrl("/proxy"));
+        assertThat(content, is("proxy_target"));
+    }
+}
