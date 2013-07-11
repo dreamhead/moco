9ae4111529681f77139998bd8d69e3116842b431
dreamhead <dreamhead.cn@gmail.com>
2013-07-12 06:30:40 +0800

    removed unused WritableResource

--- moco-core/src/main/java/com/github/dreamhead/moco/resource/FileResource.java
+++ moco-core/src/main/java/com/github/dreamhead/moco/resource/FileResource.java
@@ -2,7 +2,6 @@ package com.github.dreamhead.moco.resource;
 
 import com.github.dreamhead.moco.MocoConfig;
 import com.github.dreamhead.moco.util.FileContentType;
-import com.google.common.io.Files;
 import org.jboss.netty.handler.codec.http.HttpRequest;
 
 import java.io.File;
@@ -10,7 +9,7 @@ import java.io.IOException;
 
 import static com.google.common.io.Files.toByteArray;
 
-public class FileResource implements WritableResource, ContentResource {
+public class FileResource implements ContentResource {
     private final File file;
 
     public FileResource(File file) {
@@ -37,15 +36,6 @@ public class FileResource implements WritableResource, ContentResource {
            return new FileResource(new File(config.apply(file.getName())));
         }
         return this;
-    }
-
-    @Override
-    public void write(byte[] content) {
-        try {
-            Files.write(content, file);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
     }
 
     @Override
--- moco-core/src/main/java/com/github/dreamhead/moco/resource/WritableResource.java
+++
@@ -1,5 +0,0 @@
-package com.github.dreamhead.moco.resource;
-
-public interface WritableResource extends Resource {
-    public void write(byte[] content);
-}
