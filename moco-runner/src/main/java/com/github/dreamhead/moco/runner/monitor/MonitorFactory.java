c8e1d2a7332b8683b9cfda404fde46182f178490
dreamhead <dreamhead.cn@gmail.com>
2013-08-29 18:18:04 +0800

    replace list with immutable list in create setting monitor

--- moco-runner/src/main/java/com/github/dreamhead/moco/runner/monitor/MonitorFactory.java
+++ moco-runner/src/main/java/com/github/dreamhead/moco/runner/monitor/MonitorFactory.java
@@ -1,15 +1,14 @@
 package com.github.dreamhead.moco.runner.monitor;
 
-import com.github.dreamhead.moco.runner.*;
-import com.google.common.collect.Iterables;
+import com.github.dreamhead.moco.runner.FileRunner;
+import com.github.dreamhead.moco.runner.MonitorRunner;
+import com.github.dreamhead.moco.runner.Runner;
+import com.google.common.collect.ImmutableList;
 import org.apache.commons.io.monitor.FileAlterationListenerAdaptor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.File;
-import java.util.List;
-
-import static com.google.common.collect.Lists.newArrayList;
 
 public class MonitorFactory {
     private static Logger logger = LoggerFactory.getLogger(MonitorFactory.class);
@@ -28,8 +27,7 @@ public class MonitorFactory {
     }
 
     public Monitor createSettingMonitor(final File settingsFile, final Iterable<File> configurationFiles, final FileRunner fileRunner) {
-        List<File> files = newArrayList(settingsFile);
-        Iterables.addAll(files, configurationFiles);
+        ImmutableList<File> files = ImmutableList.<File>builder().add(settingsFile).addAll(configurationFiles).build();
         return new FilesMonitor(files, createListener(fileRunner));
     }
 
