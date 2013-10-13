72058b862f23117cd8252fa0bf595436619b46ba
dreamhead <dreamhead.cn@gmail.com>
2013-10-13 17:14:38 +0800

    removed unused annnotation in http response

--- moco-core/src/main/java/com/github/dreamhead/moco/HttpResponse.java
+++ moco-core/src/main/java/com/github/dreamhead/moco/HttpResponse.java
@@ -1,9 +1,7 @@
 package com.github.dreamhead.moco;
 
-import com.fasterxml.jackson.annotation.JsonAutoDetect;
 import com.google.common.collect.ImmutableMap;
 
-@JsonAutoDetect(fieldVisibility = JsonAutoDetect.Visibility.ANY)
 public interface HttpResponse {
     String getVersion();
 
