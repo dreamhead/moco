8fa72e1fc06ff47ef57e443b49d932dd41492922
dreamhead <dreamhead.cn@gmail.com>
2013-10-20 09:20:21 +0800

    verified unexpected request

--- moco-core/src/main/java/com/github/dreamhead/moco/UnexpectedRequestMatcher.java
+++ moco-core/src/main/java/com/github/dreamhead/moco/UnexpectedRequestMatcher.java
@@ -1,4 +1,15 @@
 package com.github.dreamhead.moco;
 
-public interface UnexpectedRequestMatcher {
+import io.netty.handler.codec.http.FullHttpRequest;
+
+public class UnexpectedRequestMatcher implements RequestMatcher {
+    @Override
+    public boolean match(FullHttpRequest request) {
+        return true;
+    }
+
+    @Override
+    public RequestMatcher apply(MocoConfig config) {
+        return this;
+    }
 }
