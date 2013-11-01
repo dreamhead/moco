81e2b0a1b27df2986fee100188789756cd1f6e24
dreamhead <dreamhead.cn@gmail.com>
2013-11-01 17:48:56 +0800

    moved all request hit related code to MocoRequestHit

---
+++ moco-core/src/main/java/com/github/dreamhead/moco/MocoRequestHit.java
@@ -0,0 +1,39 @@
+package com.github.dreamhead.moco;
+
+import com.github.dreamhead.moco.monitor.DefaultRequestHit;
+import com.github.dreamhead.moco.monitor.verification.AtLeastVerification;
+import com.github.dreamhead.moco.monitor.verification.AtMostVerification;
+import com.github.dreamhead.moco.monitor.verification.TimesVerification;
+
+import static com.google.common.base.Preconditions.checkArgument;
+
+public class MocoRequestHit {
+    public static RequestHit requestHit() {
+        return new DefaultRequestHit();
+    }
+
+    public static UnexpectedRequestMatcher unexpected() {
+        return new UnexpectedRequestMatcher() {};
+    }
+
+    public static VerificationMode never() {
+        return times(0);
+    }
+
+    public static VerificationMode times(int count) {
+        checkArgument(count >= 0, "Times count must not be less than zero");
+        return new TimesVerification(count);
+    }
+
+    public static VerificationMode atLeast(int count) {
+        checkArgument(count > 0, "Times count must be greater than zero");
+        return new AtLeastVerification(count);
+    }
+
+    public static VerificationMode atMost(int count) {
+        checkArgument(count > 0, "Times count must be greater than zero");
+        return new AtMostVerification(count);
+    }
+
+    private MocoRequestHit() {}
+}
