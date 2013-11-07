a6de3957ced2f3ccf8599f8cbcc533830e9bef06
dreamhead <dreamhead.cn@gmail.com>
2013-11-07 20:13:46 +0800

    used port rather than hard code 8080

--- moco-core/src/test/java/com/github/dreamhead/moco/internal/MocoHttpServerTest.java
+++ moco-core/src/test/java/com/github/dreamhead/moco/internal/MocoHttpServerTest.java
@@ -2,18 +2,19 @@ package com.github.dreamhead.moco.internal;
 
 import org.junit.Test;
 
+import static com.github.dreamhead.moco.RemoteTestUtils.port;
 import static com.google.common.base.Optional.of;
 
 public class MocoHttpServerTest {
     @Test
     public void should_stop_stoped_server_without_exception() {
-        MocoHttpServer server = new MocoHttpServer(ActualHttpServer.createLogServer(of(8080)));
+        MocoHttpServer server = new MocoHttpServer(ActualHttpServer.createLogServer(of(port())));
         server.stop();
     }
 
     @Test
     public void should_stop_server_many_times_without_exception() {
-        MocoHttpServer server = new MocoHttpServer(ActualHttpServer.createLogServer(of(8080)));
+        MocoHttpServer server = new MocoHttpServer(ActualHttpServer.createLogServer(of(port())));
         server.start();
         server.stop();
         server.stop();
