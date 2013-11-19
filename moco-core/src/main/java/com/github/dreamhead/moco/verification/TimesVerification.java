d612babe3f9ce89ad7ff1709efe8cef9edb2bcc8
dreamhead <dreamhead.cn@gmail.com>
2013-11-20 05:40:17 +0800

    moved verification to upper package

--- moco-core/src/main/java/com/github/dreamhead/moco/verification/TimesVerification.java
+++ moco-core/src/main/java/com/github/dreamhead/moco/verification/TimesVerification.java
@@ -1,4 +1,4 @@
-package com.github.dreamhead.moco.monitor.verification;
+package com.github.dreamhead.moco.verification;
 
 public class TimesVerification extends AbstractVerification {
     public TimesVerification(int count) {
