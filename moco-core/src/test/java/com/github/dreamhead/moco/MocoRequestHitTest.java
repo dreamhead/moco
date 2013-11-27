e2e395fa8657116f0d786edbae96e4ccd1ceac7a
dreamhead <dreamhead.cn@gmail.com>
2013-11-28 07:38:10 +0800

    renamed moco monitor test to moco request hit test

--- moco-core/src/test/java/com/github/dreamhead/moco/MocoRequestHitTest.java
+++ moco-core/src/test/java/com/github/dreamhead/moco/MocoRequestHitTest.java
@@ -22,7 +22,7 @@ import static org.mockito.Matchers.any;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 
-public class MocoMonitorTest {
+public class MocoRequestHitTest {
     private MocoTestHelper helper;
     private RequestHit hit;
 
