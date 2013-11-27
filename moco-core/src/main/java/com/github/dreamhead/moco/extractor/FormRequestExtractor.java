a6ee9ebecaa4f5eb221dc91fb84af784db70d81d
dreamhead <dreamhead.cn@gmail.com>
2013-11-27 08:50:36 +0800

    added destroy method for form extractor

--- moco-core/src/main/java/com/github/dreamhead/moco/extractor/FormRequestExtractor.java
+++ moco-core/src/main/java/com/github/dreamhead/moco/extractor/FormRequestExtractor.java
@@ -4,9 +4,10 @@ import com.github.dreamhead.moco.RequestExtractor;
 import com.google.common.base.Optional;
 import io.netty.handler.codec.http.FullHttpRequest;
 import io.netty.handler.codec.http.multipart.Attribute;
-import io.netty.handler.codec.http.multipart.DefaultHttpDataFactory;
 import io.netty.handler.codec.http.multipart.HttpPostRequestDecoder;
 import io.netty.handler.codec.http.multipart.InterfaceHttpData;
+
+import java.io.IOException;
 
 import static com.google.common.base.Optional.absent;
 import static com.google.common.base.Optional.of;
@@ -20,19 +21,28 @@ public class FormRequestExtractor implements RequestExtractor<String> {
 
     @Override
     public Optional<String> extract(FullHttpRequest request) {
+        HttpPostRequestDecoder decoder = null;
         try {
-            HttpPostRequestDecoder decoder = new HttpPostRequestDecoder(new DefaultHttpDataFactory(false), request);
-            InterfaceHttpData data = decoder.getBodyHttpData(key);
-            if (data != null && data.getHttpDataType() == InterfaceHttpData.HttpDataType.Attribute) {
-                Attribute attribute = (Attribute)data;
-                return of(attribute.getValue());
-            }
-
-            return absent();
+            decoder = new HttpPostRequestDecoder(request);
+            return decodeForm(decoder);
         } catch (HttpPostRequestDecoder.IncompatibleDataDecoderException idde) {
             return absent();
         } catch (Exception e) {
             throw new RuntimeException(e);
+        } finally {
+            if (decoder != null) {
+                decoder.destroy();
+            }
         }
+    }
+
+    private Optional<String> decodeForm(HttpPostRequestDecoder decoder) throws IOException {
+        InterfaceHttpData data = decoder.getBodyHttpData(key);
+        if (data != null && data.getHttpDataType() == InterfaceHttpData.HttpDataType.Attribute) {
+            Attribute attribute = (Attribute)data;
+            return of(attribute.getValue());
+        }
+
+        return absent();
     }
 }
