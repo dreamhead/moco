16f01b0c26920adc9c4e61537ce380b63b010f46
Marcin Grzejszczak <marcin@grzejszczak.pl>
2013-12-05 14:03:03 +0100

    1. Added switch (-d) that allows user to run moco in deamon mode or not 2. Refactored names of scripts 3. Added generated log and pid files to .gitgnore 4. Added shutdown hook for deamon version of moco that deletes the pid file 5. Refactored code

--- moco-shell/gmoco_script.groovy
+++ moco-shell/gmoco_script.groovy
@@ -5,10 +5,22 @@ import static com.github.dreamhead.moco.Moco.*
 
 SERVER_PORT = this.args[0] as Integer
 SCRIPT_CONFIGURATION = "${this.args[1]}"
+GMOCO_PID_FILE_NAME = this.args.length > 2 ? this.args[2] : null
 
 println "Starting Moco on port [$SERVER_PORT] and configuration defined in script [$SCRIPT_CONFIGURATION]"
-println "Grabbing necessary dependencies. Please wait patiently"
 
+Runtime.runtime.addShutdownHook {
+    println "Shutting down..."
+    if(GMOCO_PID_FILE_NAME) {        
+        File pidFile = new File(GMOCO_PID_FILE_NAME)
+        if(pidFile.exists()){
+            println "Removing the [$GMOCO_PID_FILE_NAME] file containing the pid"
+            pidFile.delete()
+        }
+    }
+}
+
+println "Grabbing necessary dependencies. Please wait patiently"
 @Grapes(
 @Grab(group='com.github.dreamhead', module='moco-core', version='[0.9,)')
 )
