cc60411fdee5ed449b4288ac684553ce10fd9bde
dreamhead <dreamhead.cn@gmail.com>
2013-12-08 10:21:50 +0800

    extracted collection reader

---
+++ moco-runner/src/main/java/com/github/dreamhead/moco/parser/CollectionReader.java
@@ -0,0 +1,48 @@
+package com.github.dreamhead.moco.parser;
+
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.Module;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.exc.UnrecognizedPropertyException;
+import com.fasterxml.jackson.databind.type.CollectionType;
+import com.fasterxml.jackson.databind.type.TypeFactory;
+import com.github.dreamhead.moco.HttpServer;
+import com.google.common.collect.ImmutableList;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
+
+import static com.google.common.collect.ImmutableList.copyOf;
+import static java.lang.String.format;
+
+public class CollectionReader {
+    private static Logger logger = LoggerFactory.getLogger(HttpServer.class);
+
+    private final ObjectMapper mapper = new ObjectMapper();
+    private final TypeFactory factory = TypeFactory.defaultInstance();
+
+    public CollectionReader(Module... modules) {
+        for (Module module : modules) {
+            mapper.registerModule(module);
+        }
+    }
+
+    public <T> ImmutableList<T> read(InputStream is, Class<T> elementClass) {
+        try {
+            CollectionType type = factory.constructCollectionType(List.class, elementClass);
+            List<T> sessionSettings = mapper.readValue(is, type);
+            return copyOf(sessionSettings);
+        } catch (UnrecognizedPropertyException e) {
+            logger.info("Unrecognized field: {}", e.getMessage());
+            throw new RuntimeException(format("Unrecognized field [ %s ], please check!", e.getPropertyName()));
+        } catch (JsonMappingException e) {
+            logger.info("{} {}", e.getMessage(), e.getPathReference());
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
--- moco-runner/src/main/java/com/github/dreamhead/moco/parser/GlobalSettingParser.java
+++ moco-runner/src/main/java/com/github/dreamhead/moco/parser/GlobalSettingParser.java
@@ -1,34 +1,14 @@
 package com.github.dreamhead.moco.parser;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.exc.UnrecognizedPropertyException;
-import com.fasterxml.jackson.databind.type.TypeFactory;
 import com.github.dreamhead.moco.parser.model.GlobalSetting;
 import com.google.common.collect.ImmutableList;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
-import java.io.IOException;
 import java.io.InputStream;
-import java.util.List;
-
-import static com.google.common.collect.ImmutableList.copyOf;
-import static java.lang.String.format;
 
 public class GlobalSettingParser {
-    private static Logger logger = LoggerFactory.getLogger(GlobalSettingParser.class);
-    private final ObjectMapper mapper = new ObjectMapper();
-    private final TypeFactory factory = TypeFactory.defaultInstance();
+    private final CollectionReader reader = new CollectionReader();
 
     public ImmutableList<GlobalSetting> parse(InputStream is) {
-        try {
-            List<GlobalSetting> settings = mapper.readValue(is, factory.constructCollectionType(List.class, GlobalSetting.class));
-            return copyOf(settings);
-        } catch (UnrecognizedPropertyException upe) {
-            logger.info("Unrecognized field: {}", upe.getMessage());
-            throw new RuntimeException(format("Unrecognized field [ %s ], please check!", upe.getPropertyName()));
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
+        return reader.read(is, GlobalSetting.class);
     }
 }
