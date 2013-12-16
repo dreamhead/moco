4bf1fa5cb315faa9d922d086d1ae9aefef74588e
dreamhead <dreamhead.cn@gmail.com>
2013-12-17 07:25:12 +0800

    used default http response and fixed proxy content issue

--- moco-core/src/main/java/com/github/dreamhead/moco/model/DefaultHttpResponse.java
+++ moco-core/src/main/java/com/github/dreamhead/moco/model/DefaultHttpResponse.java
@@ -3,27 +3,47 @@ package com.github.dreamhead.moco.model;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import com.github.dreamhead.moco.HttpResponse;
 import com.google.common.collect.ImmutableMap;
+import io.netty.handler.codec.http.DefaultFullHttpResponse;
 import io.netty.handler.codec.http.FullHttpResponse;
+import io.netty.handler.codec.http.HttpResponseStatus;
+import io.netty.handler.codec.http.HttpVersion;
 
 import java.nio.charset.Charset;
 import java.util.Map;
 
 import static com.google.common.collect.ImmutableMap.copyOf;
 
-@JsonDeserialize(builder=DumpHttpResponse.Builder.class)
-public class DumpHttpResponse extends DumpMessage implements HttpResponse {
+@JsonDeserialize(builder=DefaultHttpResponse.Builder.class)
+public class DefaultHttpResponse implements HttpResponse {
+    private final String version;
+    private final String content;
+    private final ImmutableMap<String, String> headers;
     private final int status;
 
-    public DumpHttpResponse(String version, int status, ImmutableMap<String, String> headers, String content) {
-        super(version, content, headers);
+    public DefaultHttpResponse(String version, int status, ImmutableMap<String, String> headers, String content) {
+        this.version = version;
+        this.headers = headers;
+        this.content = content;
         this.status = status;
+    }
+
+    public String getVersion() {
+        return version;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public ImmutableMap<String, String> getHeaders() {
+        return this.headers;
     }
 
     public int getStatus() {
         return status;
     }
 
-    public static DumpHttpResponse newResponse(FullHttpResponse response) {
+    public static DefaultHttpResponse newResponse(FullHttpResponse response) {
         ImmutableMap.Builder<String, String> headerBuilder = ImmutableMap.builder();
         for (Map.Entry<String, String> entry : response.headers()) {
             headerBuilder.put(entry);
@@ -35,6 +55,16 @@ public class DumpHttpResponse extends DumpMessage implements HttpResponse {
                 .withHeaders(headerBuilder.build())
                 .withContent(response.content().toString(Charset.defaultCharset()))
                 .build();
+    }
+
+    public FullHttpResponse toFullHttpResponse() {
+        FullHttpResponse response = new DefaultFullHttpResponse(HttpVersion.valueOf(version), HttpResponseStatus.valueOf(status));
+        for (Map.Entry<String, String> entry : headers.entrySet()) {
+            response.headers().add(entry.getKey(), entry.getValue());
+        }
+
+        response.content().writeBytes(content.getBytes());
+        return response;
     }
 
     public static Builder builder() {
@@ -70,8 +100,8 @@ public class DumpHttpResponse extends DumpMessage implements HttpResponse {
             return this;
         }
 
-        public DumpHttpResponse build() {
-            return new DumpHttpResponse(version, status, headers, content);
+        public DefaultHttpResponse build() {
+            return new DefaultHttpResponse(version, status, headers, content);
         }
     }
 }
