b3e046ac055233dcb26225e43cb04b0ddbe71ef9
dreamhead <dreamhead.cn@gmail.com>
2014-02-02 09:17:54 +0800

    fixed proguard check

--- moco-runner/src/test/java/com/github/dreamhead/moco/MocoEventStandaloneTest.java
+++ moco-runner/src/test/java/com/github/dreamhead/moco/MocoEventStandaloneTest.java
@@ -1,53 +1,47 @@
 package com.github.dreamhead.moco;
 
 import com.github.dreamhead.moco.util.Idles;
-import org.junit.Before;
+import com.google.common.io.Files;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
 
-import static com.github.dreamhead.moco.Moco.*;
-import static com.github.dreamhead.moco.MocoRequestHit.once;
-import static com.github.dreamhead.moco.MocoRequestHit.requestHit;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.nio.charset.Charset;
+
 import static com.github.dreamhead.moco.RemoteTestUtils.remoteUrl;
-import static com.github.dreamhead.moco.Runner.running;
+import static org.hamcrest.CoreMatchers.containsString;
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 
 public class MocoEventStandaloneTest extends AbstractMocoStandaloneTest {
     private static final int IDLE = 1200;
 
-    private RequestHit hit;
-    private HttpServer server;
+    @Rule
+    public TemporaryFolder folder = new TemporaryFolder();
 
-    @Before
-    public void setup() {
-        hit = requestHit();
-        server = httpserver(2587, hit);
-        server.request(by(uri("/target"))).response("0XCAFEBABE");
+    @Test
+    public void should_fire_event() throws IOException {
+        runWithConfiguration("event.json");
+        File file = folder.newFile();
+        System.setOut(new PrintStream(new FileOutputStream(file)));
+        assertThat(helper.get(remoteUrl("/event")), is("post_foo"));
+        Idles.idle(IDLE);
+
+        assertThat(Files.toString(file, Charset.defaultCharset()), containsString("0XCAFEBABE"));
     }
 
     @Test
-    public void should_fire_event() throws Exception {
-        running(server, new Runnable() {
-            @Override
-            public void run() throws Exception {
-                runWithConfiguration("event.json");
-                assertThat(helper.get(remoteUrl("/event")), is("post_foo"));
-                Idles.idle(IDLE);
-            }
-        });
-        hit.verify(by(uri("/target")), once());
-    }
+    public void should_fire_get_event() throws IOException {
+        runWithConfiguration("event.json");
+        File file = folder.newFile();
+        System.setOut(new PrintStream(new FileOutputStream(file)));
+        assertThat(helper.get(remoteUrl("/get_event")), is("get_foo"));
+        Idles.idle(IDLE);
 
-    @Test
-    public void should_fire_get_event() throws Exception {
-        running(server, new Runnable() {
-            @Override
-            public void run() throws Exception {
-                runWithConfiguration("event.json");
-                assertThat(helper.get(remoteUrl("/get_event")), is("get_foo"));
-                Idles.idle(IDLE);
-            }
-        });
-        hit.verify(by(uri("/target")), once());
+        assertThat(Files.toString(file, Charset.defaultCharset()), containsString("0XCAFEBABE"));
     }
 }
