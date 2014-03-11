1a9a7533c3063b84353c851f66ca982e32862f57
dreamhead <dreamhead.cn@gmail.com>
2014-03-12 07:39:18 +0800

    added stopwatch to record elapsed time

--- moco-runner/src/main/java/com/github/dreamhead/moco/bootstrap/StartTask.java
+++ moco-runner/src/main/java/com/github/dreamhead/moco/bootstrap/StartTask.java
@@ -2,10 +2,15 @@ package com.github.dreamhead.moco.bootstrap;
 
 import com.github.dreamhead.moco.runner.Runner;
 import com.github.dreamhead.moco.runner.RunnerFactory;
+import com.google.common.base.Stopwatch;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import static com.github.dreamhead.moco.bootstrap.StartArgs.parse;
 
 public class StartTask implements BootstrapTask {
+    private Logger logger = LoggerFactory.getLogger(StartTask.class);
+
     private final RunnerFactory factory;
 
     public StartTask(String defaultShutdownKey) {
@@ -17,12 +22,16 @@ public class StartTask implements BootstrapTask {
         StartArgs startArgs = parse(args);
 
         final Runner runner = factory.createRunner(startArgs);
+
+        final Stopwatch stopwatch = Stopwatch.createStarted();
         runner.run();
 
         Runtime.getRuntime().addShutdownHook(new Thread() {
             @Override
             public void run() {
                 runner.stop();
+                stopwatch.stop();
+                logger.info("Total time: " + stopwatch);
             }
         });
     }
