1ec2d03999c28901e7469971beac562890bd7cda
dreamhead <dreamhead.cn@gmail.com>
2014-05-12 07:16:47 +0800

    added precondition check for mount

--- moco-core/src/main/java/com/github/dreamhead/moco/MocoMount.java
+++ moco-core/src/main/java/com/github/dreamhead/moco/MocoMount.java
@@ -3,14 +3,17 @@ package com.github.dreamhead.moco;
 import com.github.dreamhead.moco.mount.MountPredicate;
 import com.github.dreamhead.moco.mount.MountTo;
 
+import static com.github.dreamhead.moco.util.Preconditions.checkNotNullOrEmpty;
+import static com.google.common.base.Preconditions.checkNotNull;
 import static org.apache.commons.io.FilenameUtils.wildcardMatch;
 
 public class MocoMount {
     public static MountTo to(final String target) {
-        return new MountTo(target);
+        return new MountTo(checkNotNullOrEmpty(target, "Target should not be null"));
     }
 
     public static MountPredicate include(final String wildcard) {
+        checkNotNullOrEmpty(wildcard, "Wildcard should not be null");
         return new MountPredicate() {
             @Override
             public boolean apply(String filename) {
@@ -20,10 +23,11 @@ public class MocoMount {
     }
 
     public static MountPredicate exclude(final String wildcard) {
-        return not(include(wildcard));
+        return not(include(checkNotNullOrEmpty(wildcard, "Wildcard should not be null")));
     }
 
     private static MountPredicate not(final MountPredicate predicate) {
+        checkNotNull(predicate);
         return new MountPredicate() {
             @Override
             public boolean apply(String filename) {
