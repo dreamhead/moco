f7efdfea4241970250b39dbf6b53f90ab6a82172
dreamhead <dreamhead.cn@gmail.com>
2014-07-09 07:45:03 +0800

    used request extractor interface for content extractor

--- moco-core/src/main/java/com/github/dreamhead/moco/ResponseHandler.java
+++ moco-core/src/main/java/com/github/dreamhead/moco/ResponseHandler.java
@@ -1,8 +1,6 @@
 package com.github.dreamhead.moco;
 
 import com.github.dreamhead.moco.internal.SessionContext;
-import io.netty.handler.codec.http.FullHttpRequest;
-import io.netty.handler.codec.http.FullHttpResponse;
 
 public interface ResponseHandler extends ConfigApplier<ResponseHandler> {
     void writeToResponse(final SessionContext context);
--- moco-core/src/main/java/com/github/dreamhead/moco/extractor/ContentRequestExtractor.java
+++ moco-core/src/main/java/com/github/dreamhead/moco/extractor/ContentRequestExtractor.java
@@ -1,15 +1,15 @@
 package com.github.dreamhead.moco.extractor;
 
-import com.github.dreamhead.moco.HttpRequest;
-import com.github.dreamhead.moco.HttpRequestExtractor;
+import com.github.dreamhead.moco.Request;
+import com.github.dreamhead.moco.RequestExtractor;
 import com.google.common.base.Optional;
 
 import static com.google.common.base.Optional.of;
 import static com.google.common.base.Strings.isNullOrEmpty;
 
-public class ContentRequestExtractor extends HttpRequestExtractor<String> {
+public class ContentRequestExtractor implements RequestExtractor<String> {
     @Override
-    protected Optional<String> doExtract(HttpRequest request) {
+    public Optional<String> extract(final Request request) {
         String content = request.getContent();
         return isNullOrEmpty(content) ? Optional.<String>absent() : of(content);
     }
