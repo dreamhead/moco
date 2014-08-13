659c38a624821c385b247eca9b28cff7042683f8
dreamhead <dreamhead.cn@gmail.com>
2014-08-13 21:28:35 +0800

    introduced base response setting configuration

---
+++ moco-core/src/main/java/com/github/dreamhead/moco/internal/BaseResponseSettingConfiguration.java
@@ -0,0 +1,51 @@
+package com.github.dreamhead.moco.internal;
+
+import com.github.dreamhead.moco.*;
+import com.github.dreamhead.moco.handler.AndResponseHandler;
+import com.github.dreamhead.moco.resource.Resource;
+
+import java.util.List;
+
+import static com.github.dreamhead.moco.Moco.text;
+import static com.github.dreamhead.moco.Moco.with;
+import static com.github.dreamhead.moco.util.Preconditions.checkNotNullOrEmpty;
+import static com.google.common.base.Preconditions.checkNotNull;
+import static com.google.common.collect.ImmutableList.copyOf;
+import static com.google.common.collect.Lists.newArrayList;
+
+public abstract class BaseResponseSettingConfiguration<T extends ResponseSetting<T>> {
+    protected abstract T self();
+
+    protected ResponseHandler handler;
+    protected List<MocoEventTrigger> eventTriggers = newArrayList();
+
+    public T response(final String content) {
+        return this.response(text(checkNotNullOrEmpty(content, "Content should not be null")));
+    }
+
+    public T response(final Resource resource) {
+        return this.response(with(checkNotNull(resource, "Resource should not be null")));
+    }
+
+    public T response(final MocoProcedure procedure) {
+        return this.response(with(checkNotNull(procedure, "Procedure should not be null")));
+    }
+
+    public T response(final ResponseHandler... handlers) {
+        return this.response(new AndResponseHandler(copyOf(handlers)));
+    }
+
+    public T response(final ResponseHandler handler) {
+        if (this.handler != null) {
+            throw new RuntimeException("handler has already been set");
+        }
+
+        this.handler = checkNotNull(handler, "Handler should not be null");
+        return self();
+    }
+
+    public T on(final MocoEventTrigger trigger) {
+        this.eventTriggers.add(checkNotNull(trigger, "Trigger should not be null"));
+        return self();
+    }
+}
--- moco-core/src/main/java/com/github/dreamhead/moco/internal/HttpResponseSettingConfiguration.java
+++ moco-core/src/main/java/com/github/dreamhead/moco/internal/HttpResponseSettingConfiguration.java
@@ -1,57 +1,20 @@
 package com.github.dreamhead.moco.internal;
 
-import com.github.dreamhead.moco.*;
-import com.github.dreamhead.moco.handler.AndResponseHandler;
-import com.github.dreamhead.moco.resource.Resource;
+import com.github.dreamhead.moco.HttpResponseSetting;
+import com.github.dreamhead.moco.RequestMatcher;
 import io.netty.handler.codec.http.HttpResponseStatus;
-
-import java.util.List;
 
 import static com.github.dreamhead.moco.Moco.*;
 import static com.github.dreamhead.moco.util.Preconditions.checkNotNullOrEmpty;
-import static com.google.common.base.Preconditions.checkNotNull;
-import static com.google.common.collect.ImmutableList.copyOf;
-import static com.google.common.collect.Lists.newArrayList;
 
-public class HttpResponseSettingConfiguration implements HttpResponseSetting {
-    protected ResponseHandler handler;
-    protected List<MocoEventTrigger> eventTriggers = newArrayList();
-
-    public HttpResponseSetting response(final String content) {
-        return this.response(text(checkNotNullOrEmpty(content, "Content should not be null")));
-    }
-
-    public HttpResponseSetting response(final Resource resource) {
-        return this.response(with(checkNotNull(resource, "Resource should not be null")));
-    }
-
-    public HttpResponseSetting response(final ResponseHandler handler) {
-        if (this.handler != null) {
-            throw new RuntimeException("handler has already been set");
-        }
-
-        this.handler = checkNotNull(handler, "Handler should not be null");
-        return this;
-    }
-
-    public HttpResponseSetting response(final MocoProcedure procedure) {
-        this.response(with(checkNotNull(procedure, "Procedure should not be null")));
-        return this;
-    }
-
-    public HttpResponseSetting response(final ResponseHandler... handlers) {
-        this.response(new AndResponseHandler(copyOf(handlers)));
+public class HttpResponseSettingConfiguration extends BaseResponseSettingConfiguration<HttpResponseSetting> implements HttpResponseSetting {
+    @Override
+    protected HttpResponseSetting self() {
         return this;
     }
 
     public HttpResponseSetting redirectTo(final String url) {
-        this.response(status(HttpResponseStatus.FOUND.code()), header("Location", checkNotNullOrEmpty(url, "URL should not be null")));
-        return this;
-    }
-
-    public HttpResponseSetting on(final MocoEventTrigger trigger) {
-        this.eventTriggers.add(checkNotNull(trigger, "Trigger should not be null"));
-        return this;
+        return this.response(status(HttpResponseStatus.FOUND.code()), header("Location", checkNotNullOrEmpty(url, "URL should not be null")));
     }
 
     protected static RequestMatcher context(final String context) {
