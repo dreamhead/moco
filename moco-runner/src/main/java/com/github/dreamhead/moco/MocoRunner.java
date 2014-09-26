4a0a68ace8785a25fcef720253f115680f7a9421
dreamhead <dreamhead.cn@gmail.com>
2014-09-26 23:39:19 +0800

    added missing final in moco runner

--- moco-runner/src/main/java/com/github/dreamhead/moco/MocoRunner.java
+++ moco-runner/src/main/java/com/github/dreamhead/moco/MocoRunner.java
@@ -11,35 +11,35 @@ import java.io.InputStream;
 import static com.google.common.base.Optional.of;
 
 public class MocoRunner {
-    public static HttpServer jsonHttpServer(int port, Resource resource) {
+    public static HttpServer jsonHttpServer(final int port, final Resource resource) {
         return jsonHttpServer(resource, of(port));
     }
 
-    public static HttpServer jsonHttpServer(Resource resource) {
+    public static HttpServer jsonHttpServer(final Resource resource) {
         Optional<Integer> port = Optional.absent();
         return jsonHttpServer(resource, port);
     }
 
-    public static SocketServer jsonSocketServer(int port, Resource resource) {
+    public static SocketServer jsonSocketServer(final int port, final Resource resource) {
         return jsonSocketServer(resource, of(port));
     }
 
-    public static SocketServer jsonSocketServer(Resource resource) {
+    public static SocketServer jsonSocketServer(final Resource resource) {
         Optional<Integer> port = Optional.absent();
         return jsonSocketServer(resource, port);
     }
 
-    private static SocketServer jsonSocketServer(Resource resource, Optional<Integer> port) {
+    private static SocketServer jsonSocketServer(final Resource resource, final Optional<Integer> port) {
         SocketServerParser parser = new SocketServerParser();
         return parser.parseServer(toStream(resource), port);
     }
 
-    private static HttpServer jsonHttpServer(Resource resource, Optional<Integer> port) {
+    private static HttpServer jsonHttpServer(final Resource resource, final Optional<Integer> port) {
         HttpServerParser parser = new HttpServerParser();
         return parser.parseServer(toStream(resource), port);
     }
 
-    private static InputStream toStream(Resource resource) {
+    private static InputStream toStream(final Resource resource) {
         Optional<Request> request = Optional.absent();
         return new ByteArrayInputStream(resource.readFor(request));
     }
