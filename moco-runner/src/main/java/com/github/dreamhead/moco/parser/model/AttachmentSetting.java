e0b82123316d4fc081331fe9cad2260de041e083
dreamhead <dreamhead.cn@gmail.com>
2014-11-02 09:48:56 +0800

    added toString to attachement setting

--- moco-runner/src/main/java/com/github/dreamhead/moco/parser/model/AttachmentSetting.java
+++ moco-runner/src/main/java/com/github/dreamhead/moco/parser/model/AttachmentSetting.java
@@ -2,13 +2,24 @@ package com.github.dreamhead.moco.parser.model;
 
 import com.fasterxml.jackson.annotation.JsonAutoDetect;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.google.common.base.MoreObjects;
 
 @JsonAutoDetect(fieldVisibility = JsonAutoDetect.Visibility.ANY)
 public class AttachmentSetting extends BaseResourceSetting {
     private String filename;
 
-
     public String getFilename() {
         return filename;
+    }
+
+    @Override
+    public String toString() {
+        return MoreObjects.toStringHelper(this)
+                .omitNullValues()
+                .add("filename", filename)
+                .add("text", text)
+                .add("file", file)
+                .add("path resource", pathResource)
+                .toString();
     }
 }
