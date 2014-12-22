6cb27ae4fe7bc2cba8acb1dacabd4e95281d07fc
dreamhead <dreamhead.cn@gmail.com>
2014-12-22 20:49:22 +0800

    removed unused message factory

--- moco-core/src/main/java/com/github/dreamhead/moco/util/Idles.java
+++ moco-core/src/main/java/com/github/dreamhead/moco/util/Idles.java
@@ -3,7 +3,6 @@ package com.github.dreamhead.moco.util;
 import java.util.concurrent.TimeUnit;
 
 public class Idles {
-
     public static void idle(long duration, TimeUnit unit) {
         try {
             unit.sleep(duration);
