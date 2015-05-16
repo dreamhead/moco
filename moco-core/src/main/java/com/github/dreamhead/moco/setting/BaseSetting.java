f3028873d9ec49df9380ad0ef34a3684da51edd3
dreamhead <dreamhead.cn@gmail.com>
2015-05-16 23:03:48 +0800

    implemented setting interface for base setting

--- moco-core/src/main/java/com/github/dreamhead/moco/setting/BaseSetting.java
+++ moco-core/src/main/java/com/github/dreamhead/moco/setting/BaseSetting.java
@@ -4,17 +4,20 @@ import com.github.dreamhead.moco.*;
 import com.github.dreamhead.moco.internal.BaseResponseSettingConfiguration;
 import com.github.dreamhead.moco.internal.SessionContext;
 
-public abstract class BaseSetting<T extends ResponseSetting<T>> extends BaseResponseSettingConfiguration<T> {
+public abstract class BaseSetting<T extends ResponseSetting<T>>
+        extends BaseResponseSettingConfiguration<T> implements Setting<T> {
     protected final RequestMatcher matcher;
 
     public BaseSetting(final RequestMatcher matcher) {
         this.matcher = matcher;
     }
 
+    @Override
     public boolean match(Request request) {
         return this.matcher.match(request) && this.handler != null;
     }
 
+    @Override
     public void writeToResponse(SessionContext context) {
         this.handler.writeToResponse(context);
         this.fireCompleteEvent();
