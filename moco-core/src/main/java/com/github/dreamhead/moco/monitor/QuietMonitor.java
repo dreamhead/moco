947a4849732903f76408af20d3cb4c5f167334ec
dreamhead <dreamhead.cn@gmail.com>
2015-06-13 21:32:16 +0800

    outputed exception to system error in quite monitor

--- moco-core/src/main/java/com/github/dreamhead/moco/monitor/QuietMonitor.java
+++ moco-core/src/main/java/com/github/dreamhead/moco/monitor/QuietMonitor.java
@@ -1,13 +1,8 @@
 package com.github.dreamhead.moco.monitor;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 public class QuietMonitor extends AbstractMonitor {
-    private static Logger logger = LoggerFactory.getLogger(QuietMonitor.class);
-
     @Override
     public void onException(final Throwable t) {
-        logger.error("Exception thrown", t);
+        t.printStackTrace(System.err);
     }
 }
