f1ef1aa19c5470b039baff649028287446417d36
dreamhead <dreamhead.cn@gmail.com>
2015-06-30 23:17:41 +0800

    selected request dumper in log formatter

--- moco-core/src/main/java/com/github/dreamhead/moco/monitor/DefaultLogFormatter.java
+++ moco-core/src/main/java/com/github/dreamhead/moco/monitor/DefaultLogFormatter.java
@@ -1,26 +1,29 @@
 package com.github.dreamhead.moco.monitor;
 
+import com.github.dreamhead.moco.HttpRequest;
+import com.github.dreamhead.moco.HttpResponse;
 import com.github.dreamhead.moco.Request;
 import com.github.dreamhead.moco.Response;
+import com.github.dreamhead.moco.SocketRequest;
+import com.github.dreamhead.moco.SocketResponse;
 import com.github.dreamhead.moco.dumper.Dumper;
 import com.github.dreamhead.moco.dumper.HttpRequestDumper;
 import com.github.dreamhead.moco.dumper.HttpResponseDumper;
+import com.github.dreamhead.moco.dumper.SocketRequestDumper;
+import com.github.dreamhead.moco.dumper.SocketResponseDumper;
 
 import java.io.PrintWriter;
 import java.io.StringWriter;
 
 public class DefaultLogFormatter implements LogFormatter {
-    private final Dumper<Request> requestDumper = new HttpRequestDumper();
-    private final Dumper<Response> responseDumper = new HttpResponseDumper();
-
     @Override
     public String format(final Request request) {
-        return String.format("Request received:\n\n%s\n", requestDumper.dump(request));
+        return String.format("Request received:\n\n%s\n", requestDumper(request).dump(request));
     }
 
     @Override
     public String format(final Response response) {
-        return String.format("Response return:\n\n%s\n", responseDumper.dump(response));
+        return String.format("Response return:\n\n%s\n", responseDumper(response).dump(response));
     }
 
     @Override
@@ -32,5 +35,29 @@ public class DefaultLogFormatter implements LogFormatter {
         StringWriter writer = new StringWriter();
         e.printStackTrace(new PrintWriter(writer));
         return writer.toString();
+    }
+
+    private Dumper<Request> requestDumper(final Request request) {
+        if (request instanceof HttpRequest) {
+            return new HttpRequestDumper();
+        }
+
+        if (request instanceof SocketRequest) {
+            return new SocketRequestDumper();
+        }
+
+        throw new IllegalArgumentException("Unknown request type:" + request.getClass());
+    }
+
+    private Dumper<Response> responseDumper(final Response response) {
+        if (response instanceof HttpResponse) {
+            return new HttpResponseDumper();
+        }
+
+        if (response instanceof SocketResponse) {
+            return new SocketResponseDumper();
+        }
+
+        throw new IllegalArgumentException("Unknown response type:" + response.getClass());
     }
 }
