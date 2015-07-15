e40be0e4d4f3870a25d4c9fa1d9996ce0f11f101
dreamhead <dreamhead.cn@gmail.com>
2015-07-15 11:38:52 +0800

    replaced for iteration in default log fommatter

--- moco-core/src/main/java/com/github/dreamhead/moco/monitor/DefaultLogFormatter.java
+++ moco-core/src/main/java/com/github/dreamhead/moco/monitor/DefaultLogFormatter.java
@@ -11,10 +11,14 @@ import com.github.dreamhead.moco.dumper.HttpRequestDumper;
 import com.github.dreamhead.moco.dumper.HttpResponseDumper;
 import com.github.dreamhead.moco.dumper.SocketRequestDumper;
 import com.github.dreamhead.moco.dumper.SocketResponseDumper;
+import com.google.common.base.Optional;
+import com.google.common.base.Predicate;
 import com.google.common.collect.ImmutableMap;
 
 import java.io.PrintWriter;
 import java.io.StringWriter;
+
+import static com.google.common.collect.FluentIterable.from;
 
 public class DefaultLogFormatter implements LogFormatter {
     private static ImmutableMap<Class<? extends Request>, Dumper<Request>> REQUEST_DUMPERS = ImmutableMap.of(
@@ -48,13 +52,21 @@ public class DefaultLogFormatter implements LogFormatter {
         return writer.toString();
     }
 
-    private <T> Dumper<T> findDumper(final T target, ImmutableMap<Class<? extends T>, Dumper<T>> dumerClasses) {
-        for (Class<? extends T> dumperClass : dumerClasses.keySet()) {
-            if (dumperClass.isInstance(target)) {
-                return dumerClasses.get(dumperClass);
-            }
+    private <T> Dumper<T> findDumper(final T target, final ImmutableMap<Class<? extends T>, Dumper<T>> dumperClasses) {
+        Optional<Class<? extends T>> dumpClass = from(dumperClasses.keySet()).firstMatch(isInstance(target));
+        if (dumpClass.isPresent()) {
+            return dumperClasses.get(dumpClass.get());
         }
 
         throw new IllegalArgumentException("Unknown target type:" + target.getClass());
+    }
+
+    private <T> Predicate<Class<? extends T>> isInstance(final T target) {
+        return new Predicate<Class<? extends T>>() {
+            @Override
+            public boolean apply(final Class<? extends T> input) {
+                return input.isInstance(target);
+            }
+        };
     }
 }
