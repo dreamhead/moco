bf7f98d46a84759aa176e79f6d03a03875cf0b7a
dreamhead <dreamhead.cn@gmail.com>
2015-07-23 17:57:20 +0800

    upgraded jackson to 2.6.0

--- moco-core/src/test/java/com/github/dreamhead/moco/util/FileContentTypeTest.java
+++ moco-core/src/test/java/com/github/dreamhead/moco/util/FileContentTypeTest.java
@@ -6,7 +6,6 @@ import org.junit.Test;
 
 import java.nio.charset.Charset;
 
-import static com.google.common.base.Optional.absent;
 import static com.google.common.base.Optional.of;
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
