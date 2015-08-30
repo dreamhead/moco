27b1447919502b4c594323850beee5ee21a33cf4
dreamhead <dreamhead.cn@gmail.com>
2015-08-30 21:28:26 +0800

    bumped version to 0.10.2

--- moco-core/src/main/java/com/github/dreamhead/moco/MocoVersion.java
+++ moco-core/src/main/java/com/github/dreamhead/moco/MocoVersion.java
@@ -1,7 +1,7 @@
 package com.github.dreamhead.moco;
 
 public final class MocoVersion {
-    public static final String VERSION = "0.10.1";
+    public static final String VERSION = "0.10.2";
 
     private MocoVersion() {}
 }
