0c8bee998f19babddd8453031901b3c2bb74467d
dreamhead <dreamhead.cn@gmail.com>
2015-09-20 22:04:03 +0800

    clean up junit runner

--- moco-junit/src/main/java/com/github/dreamhead/moco/junit/MocoJunitRunner.java
+++ moco-junit/src/main/java/com/github/dreamhead/moco/junit/MocoJunitRunner.java
@@ -1,12 +1,14 @@
 package com.github.dreamhead.moco.junit;
 
 import com.github.dreamhead.moco.HttpServer;
-import com.github.dreamhead.moco.MocoJsonRunner;
 import com.github.dreamhead.moco.Runner;
 import com.github.dreamhead.moco.SocketServer;
 import org.junit.rules.ExternalResource;
 
 import static com.github.dreamhead.moco.Moco.file;
+import static com.github.dreamhead.moco.MocoJsonRunner.jsonHttpServer;
+import static com.github.dreamhead.moco.MocoJsonRunner.jsonSocketServer;
+import static com.github.dreamhead.moco.Runner.runner;
 
 public final class MocoJunitRunner extends ExternalResource {
     private final Runner runner;
@@ -16,19 +18,19 @@ public final class MocoJunitRunner extends ExternalResource {
     }
 
     public static MocoJunitRunner jsonHttpRunner(final int port, final String filename) {
-        return new MocoJunitRunner(Runner.runner(MocoJsonRunner.jsonHttpServer(port, file(filename))));
+        return new MocoJunitRunner(runner(jsonHttpServer(port, file(filename))));
     }
 
     public static MocoJunitRunner httpRunner(final HttpServer server) {
-        return new MocoJunitRunner(Runner.runner(server));
+        return new MocoJunitRunner(runner(server));
     }
 
     public static MocoJunitRunner jsonSocketRunner(final int port, final String filename) {
-        return new MocoJunitRunner(Runner.runner(MocoJsonRunner.jsonSocketServer(port, file(filename))));
+        return new MocoJunitRunner(runner(jsonSocketServer(port, file(filename))));
     }
 
     public static MocoJunitRunner socketRunner(final SocketServer server) {
-        return new MocoJunitRunner(Runner.runner(server));
+        return new MocoJunitRunner(runner(server));
     }
 
     @Override
