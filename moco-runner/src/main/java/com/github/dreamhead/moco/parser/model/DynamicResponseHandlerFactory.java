bf6de4e6a9b51ace4145909944f02e3bc26f47cc
dreamhead <dreamhead.cn@gmail.com>
2015-10-05 05:47:01 +0800

    removed unused unchecked warning in dynamic response handler factory

--- moco-runner/src/main/java/com/github/dreamhead/moco/parser/model/DynamicResponseHandlerFactory.java
+++ moco-runner/src/main/java/com/github/dreamhead/moco/parser/model/DynamicResponseHandlerFactory.java
@@ -220,7 +220,6 @@ public class DynamicResponseHandlerFactory extends Dynamics implements ResponseH
     private static Maps.EntryTransformer<String, TextContainer, RequestExtractor<?>> toVariable() {
         return new Maps.EntryTransformer<String, TextContainer, RequestExtractor<?>>() {
             @Override
-            @SuppressWarnings("unchecked")
             public RequestExtractor<?> transformEntry(final String key, final TextContainer value) {
                 if (value.isRawText()) {
                     return var(value.getText());
