63e4c6e9f37f7897df43c226b2afdb133bd65385
dreamhead <dreamhead.cn@gmail.com>
2015-10-15 21:51:44 +0800

    upgraded jackson to 2.6.3

--- moco-runner/src/main/java/com/github/dreamhead/moco/parser/model/DynamicRequestMatcherFactory.java
+++ moco-runner/src/main/java/com/github/dreamhead/moco/parser/model/DynamicRequestMatcherFactory.java
@@ -113,6 +113,7 @@ public class DynamicRequestMatcherFactory extends Dynamics implements RequestMat
     private Function<Map.Entry<String, Object>, RequestMatcher> toTargetMatcher(final Method extractorMethod) {
         return new Function<Map.Entry<String, Object>, RequestMatcher>() {
             @Override
+            @SuppressWarnings("unchecked")
             public RequestMatcher apply(final Map.Entry<String, Object> pair) {
                 RequestExtractor extractor = createRequestExtractor(extractorMethod, pair.getKey());
                 return createRequestMatcher(extractor, pair.getValue());
