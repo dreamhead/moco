efe7b6017a12a04439ce8738638f0c85b0d8f11b
dreamhead <dreamhead.cn@gmail.com>
2015-11-10 21:48:51 +0800

    added missing check for rest

--- moco-core/src/main/java/com/github/dreamhead/moco/MocoRest.java
+++ moco-core/src/main/java/com/github/dreamhead/moco/MocoRest.java
@@ -5,23 +5,28 @@ import com.github.dreamhead.moco.rest.ActualRestServer;
 import com.github.dreamhead.moco.rest.RestSetting;
 import com.google.common.base.Optional;
 
+import static com.github.dreamhead.moco.util.Preconditions.checkNotNullOrEmpty;
 import static com.google.common.base.Optional.of;
 import static com.google.common.base.Preconditions.checkArgument;
 import static com.google.common.base.Preconditions.checkNotNull;
 
 public final class MocoRest {
     public static RestServer restServer(final int port, final MocoConfig... configs) {
+        checkArgument(port > 0, "Port must be greater than zero");
+        checkNotNull(configs, "Config should not be null");
         return new ActualRestServer(of(port), Optional.<HttpsCertificate>absent(), new QuietMonitor(), configs);
     }
 
     public static RestServer restServer(final int port, final MocoMonitor monitor, final MocoConfig... configs) {
         checkArgument(port > 0, "Port must be greater than zero");
+        checkNotNull(configs, "Config should not be null");
         return new ActualRestServer(of(port), Optional.<HttpsCertificate>absent(),
                 checkNotNull(monitor, "Monitor should not be null"), configs);
     }
 
     public static RestSetting get(final String id, final ResponseHandler handler) {
-        return new RestSetting(id, handler);
+        return new RestSetting(checkNotNullOrEmpty(id, "ID should not be null or empty"),
+                checkNotNull(handler, "Get response handler should not be null"));
     }
 
     private MocoRest() {
