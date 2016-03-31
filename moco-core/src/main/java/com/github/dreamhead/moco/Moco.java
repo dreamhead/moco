55dd880b6d664c8b37e65b15a4fe9a21ba4e45a7
dreamhead <dreamhead.cn@gmail.com>
2016-03-31 22:34:07 +0800

    fixed xml and json precondition

--- moco-core/src/main/java/com/github/dreamhead/moco/Moco.java
+++ moco-core/src/main/java/com/github/dreamhead/moco/Moco.java
@@ -364,7 +364,7 @@ public final class Moco {
     }
 
     public static RequestMatcher xml(final String resource) {
-        return xml(text(checkNotNull(resource, "Resource should not be null")));
+        return xml(text(checkNotNullOrEmpty(resource, "Resource should not be null")));
     }
 
     public static RequestMatcher xml(final Resource resource) {
@@ -373,7 +373,7 @@ public final class Moco {
     }
 
     public static RequestMatcher json(final String jsonText) {
-        return json(text(checkNotNull(jsonText, "Json should not be null")));
+        return json(text(checkNotNullOrEmpty(jsonText, "Json should not be null")));
     }
 
     public static RequestMatcher json(final Object pojo) {
