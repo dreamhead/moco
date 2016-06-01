fc4d25a8f0bf0da6a88b5798f4de425bb55335f7
dreamhead <dreamhead.cn@gmail.com>
2016-06-01 21:57:24 +0800

    Retrived moco version from manifest

--- moco-core/src/main/java/com/github/dreamhead/moco/MocoVersion.java
+++ moco-core/src/main/java/com/github/dreamhead/moco/MocoVersion.java
@@ -1,7 +1,11 @@
 package com.github.dreamhead.moco;
 
 public final class MocoVersion {
-    public static final String VERSION = "0.10.3";
+    public static final String VERSION;
+
+    static {
+        VERSION = MocoVersion.class.getPackage().getImplementationVersion();
+    }
 
     private MocoVersion() {
     }
