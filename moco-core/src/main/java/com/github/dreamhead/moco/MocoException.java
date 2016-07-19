7799e31ab953ee8a07cf1fc6ddeb19da646f0998
dreamhead <dreamhead.cn@gmail.com>
2016-07-19 21:25:40 +0800

    added missing final to moco exception

--- moco-core/src/main/java/com/github/dreamhead/moco/MocoException.java
+++ moco-core/src/main/java/com/github/dreamhead/moco/MocoException.java
@@ -1,19 +1,16 @@
 package com.github.dreamhead.moco;
 
-/**
- * Created by DevFactory on 1/22/16.
- */
 public class MocoException extends RuntimeException {
 
     public MocoException(final String message) {
         super(message);
     }
 
-    public MocoException(Throwable cause) {
+    public MocoException(final Throwable cause) {
         super(cause);
     }
 
-    public MocoException(String message, Throwable cause) {
+    public MocoException(final String message, final Throwable cause) {
         super(message, cause);
     }
 
