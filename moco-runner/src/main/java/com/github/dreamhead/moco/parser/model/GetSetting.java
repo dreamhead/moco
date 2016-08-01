07571f693b4a81c20a9cc1abc7144c81b2c1a75c
dreamhead <dreamhead.cn@gmail.com>
2016-08-01 20:54:24 +0800

    extracted url resource from get and post setting for stanadlone mode

--- moco-runner/src/main/java/com/github/dreamhead/moco/parser/model/GetSetting.java
+++ moco-runner/src/main/java/com/github/dreamhead/moco/parser/model/GetSetting.java
@@ -2,10 +2,12 @@ package com.github.dreamhead.moco.parser.model;
 
 import com.fasterxml.jackson.annotation.JsonAutoDetect;
 import com.github.dreamhead.moco.MocoEventAction;
+import com.github.dreamhead.moco.resource.Resource;
 import com.google.common.base.MoreObjects;
 
 import static com.github.dreamhead.moco.Moco.get;
 import static com.github.dreamhead.moco.Moco.template;
+import static com.github.dreamhead.moco.Moco.text;
 import static com.github.dreamhead.moco.parser.model.DynamicResponseHandlerFactory.toVariables;
 
 @JsonAutoDetect(fieldVisibility = JsonAutoDetect.Visibility.ANY)
@@ -13,12 +15,16 @@ public class GetSetting {
     private TextContainer url;
 
     public MocoEventAction createAction() {
+        return get(urlResource(url));
+    }
+
+    private Resource urlResource(final TextContainer url) {
         if (url.isRawText()) {
-            return get(url.getText());
+            return text(url.getText());
         }
 
         if (url.isForTemplate()) {
-            return get(template(url.getText(), toVariables(url.getProps())));
+            return template(url.getText(), toVariables(url.getProps()));
         }
 
         throw new IllegalArgumentException("Unknown " + url + " for get setting");
--- moco-runner/src/main/java/com/github/dreamhead/moco/parser/model/PostSetting.java
+++ moco-runner/src/main/java/com/github/dreamhead/moco/parser/model/PostSetting.java
@@ -3,11 +3,13 @@ package com.github.dreamhead.moco.parser.model;
 import com.fasterxml.jackson.annotation.JsonAutoDetect;
 import com.github.dreamhead.moco.Moco;
 import com.github.dreamhead.moco.MocoEventAction;
+import com.github.dreamhead.moco.resource.Resource;
 import com.google.common.base.MoreObjects;
 
 import static com.github.dreamhead.moco.Moco.get;
 import static com.github.dreamhead.moco.Moco.post;
 import static com.github.dreamhead.moco.Moco.template;
+import static com.github.dreamhead.moco.Moco.text;
 import static com.github.dreamhead.moco.parser.model.DynamicResponseHandlerFactory.toVariables;
 
 @JsonAutoDetect(fieldVisibility = JsonAutoDetect.Visibility.ANY)
@@ -16,15 +18,19 @@ public class PostSetting {
     private String content;
 
     public MocoEventAction createAction() {
+        return post(urlResource(this.url), Moco.text(content));
+    }
+
+    private Resource urlResource(final TextContainer url) {
         if (url.isRawText()) {
-            return post(url.getText(), Moco.text(content));
+            return text(url.getText());
         }
 
         if (url.isForTemplate()) {
-            return post(template(url.getText(), toVariables(url.getProps())), Moco.text(content));
+            return template(url.getText(), toVariables(url.getProps()));
         }
 
-        throw new IllegalArgumentException("Unknown " + url + " for get setting");
+        throw new IllegalArgumentException("Unknown " + url + " for post setting");
     }
 
     @Override
