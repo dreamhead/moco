e30cec4703dd6711649de19b10d03e02432c0d0c
dreamhead <dreamhead.cn@gmail.com>
2016-09-01 22:18:16 +0800

    removed unused import in text container deserializer helper

--- moco-runner/src/main/java/com/github/dreamhead/moco/parser/deserializer/TextContainerDeserializerHelper.java
+++ moco-runner/src/main/java/com/github/dreamhead/moco/parser/deserializer/TextContainerDeserializerHelper.java
@@ -3,7 +3,6 @@ package com.github.dreamhead.moco.parser.deserializer;
 import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.core.JsonToken;
 import com.fasterxml.jackson.databind.DeserializationContext;
-import com.fasterxml.jackson.databind.JsonDeserializer;
 import com.github.dreamhead.moco.parser.model.TextContainer;
 import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.Iterators;
