44d1c7dd8ae48f8a85fad6e123f10f048de60135
dreamhead <dreamhead.cn@gmail.com>
2017-04-27 09:30:09 +0800

    added multi query test for standalone test

--- moco-runner/src/test/java/com/github/dreamhead/moco/MocoStandaloneTest.java
+++ moco-runner/src/test/java/com/github/dreamhead/moco/MocoStandaloneTest.java
@@ -159,6 +159,12 @@ public class MocoStandaloneTest extends AbstractMocoStandaloneTest {
     }
 
     @Test
+    public void should_return_expected_response_based_on_specified_multi_query_request() throws IOException {
+        runWithConfiguration("query.json");
+        assertThat(helper.get(remoteUrl("/multi-query?param1=foo&param2=bar")), is("response_for_multi_query_request"));
+    }
+
+    @Test
     public void should_expected_response_status_code() throws IOException {
         runWithConfiguration("foo.json");
         assertThat(helper.getForStatus(remoteUrl("/status")), is(200));
