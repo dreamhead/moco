c3ef91cde7b992b78f231647289eab496d3c3837
dreamhead <dreamhead.cn@gmail.com>
2017-07-14 20:11:36 +0800

    removed unused logger

--- moco-runner/src/main/java/com/github/dreamhead/moco/runner/watcher/Java7Watcher.java
+++ moco-runner/src/main/java/com/github/dreamhead/moco/runner/watcher/Java7Watcher.java
@@ -2,14 +2,11 @@ package com.github.dreamhead.moco.runner.watcher;
 
 import com.github.dreamhead.moco.MocoException;
 import com.google.common.base.Function;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import java.io.File;
 import java.io.IOException;
 
 public class Java7Watcher implements Watcher {
-    private static Logger logger = LoggerFactory.getLogger(Java7Watcher.class);
     private static WatcherService service = new WatcherService();
     private final Function<File, Void> listener;
     private final File file;
