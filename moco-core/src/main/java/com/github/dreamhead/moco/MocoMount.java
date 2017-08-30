703ba56c6e13618a37256b2e8e63178b36df18c3
dreamhead <dreamhead.cn@gmail.com>
2017-08-30 21:26:06 +0800

    replaced wildcard with glob in Java 7 for mount

--- moco-core/src/main/java/com/github/dreamhead/moco/MocoMount.java
+++ moco-core/src/main/java/com/github/dreamhead/moco/MocoMount.java
@@ -3,9 +3,12 @@ package com.github.dreamhead.moco;
 import com.github.dreamhead.moco.mount.MountPredicate;
 import com.github.dreamhead.moco.mount.MountTo;
 
+import java.nio.file.FileSystems;
+import java.nio.file.PathMatcher;
+import java.nio.file.Paths;
+
 import static com.github.dreamhead.moco.util.Preconditions.checkNotNullOrEmpty;
 import static com.google.common.base.Preconditions.checkNotNull;
-import static org.apache.commons.io.FilenameUtils.wildcardMatch;
 
 public final class MocoMount {
     public static MountTo to(final String target) {
@@ -14,10 +17,12 @@ public final class MocoMount {
 
     public static MountPredicate include(final String wildcard) {
         checkNotNullOrEmpty(wildcard, "Wildcard should not be null");
+        final PathMatcher matcher = FileSystems.getDefault().getPathMatcher("glob:" + wildcard);
+
         return new MountPredicate() {
             @Override
             public boolean apply(final String filename) {
-                return wildcardMatch(filename, wildcard);
+                return matcher.matches(Paths.get(filename));
             }
         };
     }
