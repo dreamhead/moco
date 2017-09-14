37f57731edd38057a4dbd013dd0b796219a32838
dreamhead <dreamhead.cn@gmail.com>
2017-09-14 21:07:21 +0800

    added missing final to locks

--- moco-core/src/main/java/com/github/dreamhead/moco/util/Locks.java
+++ moco-core/src/main/java/com/github/dreamhead/moco/util/Locks.java
@@ -2,7 +2,7 @@ package com.github.dreamhead.moco.util;
 
 import java.util.concurrent.locks.Lock;
 
-public class Locks {
+public final class Locks {
     public static void withLock(final Lock lock, final Runnable runnable) {
         lock.lock();
         try {
