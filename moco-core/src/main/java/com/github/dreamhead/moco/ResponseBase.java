8857ee1fde6461d1f49d0bd2d00af840b5e5ccc0
dreamhead <dreamhead.cn@gmail.com>
2018-01-13 20:54:02 +0800

    removed redundant final from response base

--- moco-core/src/main/java/com/github/dreamhead/moco/ResponseBase.java
+++ moco-core/src/main/java/com/github/dreamhead/moco/ResponseBase.java
@@ -3,8 +3,8 @@ package com.github.dreamhead.moco;
 import com.github.dreamhead.moco.resource.Resource;
 
 public interface ResponseBase<T> {
-    T response(final ResponseHandler handler, final ResponseHandler... handlers);
-    T response(final String content);
-    T response(final Resource resource);
-    T response(final MocoProcedure procedure);
+    T response(ResponseHandler handler, ResponseHandler... handlers);
+    T response(String content);
+    T response(Resource resource);
+    T response(MocoProcedure procedure);
 }
