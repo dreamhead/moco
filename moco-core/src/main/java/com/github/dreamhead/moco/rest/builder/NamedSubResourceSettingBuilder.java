b62e613e4c7581214f0c692f7bcd40fa6b81c18f
dreamhead <dreamhead.cn@gmail.com>
2018-01-15 21:27:11 +0800

    removed redundant final from named subresource setting builder

--- moco-core/src/main/java/com/github/dreamhead/moco/rest/builder/NamedSubResourceSettingBuilder.java
+++ moco-core/src/main/java/com/github/dreamhead/moco/rest/builder/NamedSubResourceSettingBuilder.java
@@ -3,5 +3,5 @@ package com.github.dreamhead.moco.rest.builder;
 import com.github.dreamhead.moco.RestSetting;
 
 public interface NamedSubResourceSettingBuilder {
-    RestSetting settings(final RestSetting restSetting, final RestSetting... restSettings);
+    RestSetting settings(RestSetting restSetting, RestSetting... restSettings);
 }
