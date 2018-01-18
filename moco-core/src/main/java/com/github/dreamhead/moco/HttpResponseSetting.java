0139eaf90c81182e8cc50f80511b327723fbe074
dreamhead <dreamhead.cn@gmail.com>
2018-01-18 22:17:35 +0800

    removed redundant final from http response setting

--- moco-core/src/main/java/com/github/dreamhead/moco/HttpResponseSetting.java
+++ moco-core/src/main/java/com/github/dreamhead/moco/HttpResponseSetting.java
@@ -3,6 +3,6 @@ package com.github.dreamhead.moco;
 import com.github.dreamhead.moco.resource.Resource;
 
 public interface HttpResponseSetting extends ResponseSetting<HttpResponseSetting> {
-    HttpResponseSetting redirectTo(final String url);
-    HttpResponseSetting redirectTo(final Resource url);
+    HttpResponseSetting redirectTo(String url);
+    HttpResponseSetting redirectTo(Resource url);
 }
