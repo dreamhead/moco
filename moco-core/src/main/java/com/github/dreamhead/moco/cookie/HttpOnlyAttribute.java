9f781465199888aee92a436d56450f8fb76cf03b
dreamhead <dreamhead.cn@gmail.com>
2018-01-24 06:49:49 +0800

    removed redundant final from http only attribute

--- moco-core/src/main/java/com/github/dreamhead/moco/cookie/HttpOnlyAttribute.java
+++ moco-core/src/main/java/com/github/dreamhead/moco/cookie/HttpOnlyAttribute.java
@@ -4,7 +4,7 @@ import io.netty.handler.codec.http.cookie.Cookie;
 
 public final class HttpOnlyAttribute extends ActualCookieAttribute {
     @Override
-    public final void visit(final Cookie cookie) {
+    public void visit(final Cookie cookie) {
         cookie.setHttpOnly(true);
     }
 }
