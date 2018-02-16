a966f9fa2f21f7609ff10ab91b794ebc97124a20
dreamhead <dreamhead.cn@gmail.com>
2018-02-16 23:25:26 +0800

    moved suppress warnings to method in file config applier

--- moco-core/src/main/java/com/github/dreamhead/moco/resource/ResourceConfigApplierFactory.java
+++ moco-core/src/main/java/com/github/dreamhead/moco/resource/ResourceConfigApplierFactory.java
@@ -51,10 +51,10 @@ public final class ResourceConfigApplierFactory {
         };
     }
 
-    @SuppressWarnings("unchecked")
     public static ResourceConfigApplier uriConfigApplier(final String id, final String uri) {
         return new SelfResourceConfigApplier(id) {
             @Override
+            @SuppressWarnings("unchecked")
             protected Resource newResource(final MocoConfig config) {
                 return uriResource((String) config.apply(uri));
             }
