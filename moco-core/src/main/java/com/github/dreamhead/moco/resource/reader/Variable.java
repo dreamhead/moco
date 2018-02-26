8984b100bd4e4b8f3bb790e8b11b420f478ded5d
dreamhead <dreamhead.cn@gmail.com>
2018-02-26 21:55:35 +0800

    removed redundant final in variable

--- moco-core/src/main/java/com/github/dreamhead/moco/resource/reader/Variable.java
+++ moco-core/src/main/java/com/github/dreamhead/moco/resource/reader/Variable.java
@@ -3,5 +3,5 @@ package com.github.dreamhead.moco.resource.reader;
 import com.github.dreamhead.moco.Request;
 
 public interface Variable {
-    Object toTemplateVariable(final Request request);
+    Object toTemplateVariable(Request request);
 }
