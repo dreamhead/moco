c1b454b55ae17b46fcdeb2b80cd0e50586ff71e8
dreamhead <dreamhead.cn@gmail.com>
2018-03-02 10:03:11 +0800

    added missing final to attachment setting

--- moco-runner/src/main/java/com/github/dreamhead/moco/parser/model/AttachmentSetting.java
+++ moco-runner/src/main/java/com/github/dreamhead/moco/parser/model/AttachmentSetting.java
@@ -4,7 +4,7 @@ import com.fasterxml.jackson.annotation.JsonAutoDetect;
 import com.google.common.base.MoreObjects;
 
 @JsonAutoDetect(fieldVisibility = JsonAutoDetect.Visibility.ANY)
-public class AttachmentSetting extends BaseResourceSetting {
+public final class AttachmentSetting extends BaseResourceSetting {
     private String filename;
 
     public String getFilename() {
