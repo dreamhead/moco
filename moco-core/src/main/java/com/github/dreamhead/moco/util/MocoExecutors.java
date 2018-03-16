8485b61390ab4862a891d4ab2ee9c15363a7d3f5
dreamhead <dreamhead.cn@gmail.com>
2018-03-16 21:29:28 +0800

    renamed executor in moco executors

--- moco-core/src/main/java/com/github/dreamhead/moco/util/MocoExecutors.java
+++ moco-core/src/main/java/com/github/dreamhead/moco/util/MocoExecutors.java
@@ -3,11 +3,11 @@ package com.github.dreamhead.moco.util;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
-public class MocoExecutors {
-    private static final ExecutorService executor = Executors.newCachedThreadPool();
+public final class MocoExecutors {
+    private static final ExecutorService EXECUTOR = Executors.newCachedThreadPool();
 
     public static ExecutorService executor() {
-        return executor;
+        return EXECUTOR;
     }
 
     private MocoExecutors() {
