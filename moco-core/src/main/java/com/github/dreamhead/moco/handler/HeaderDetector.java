f2aa03c7561c215485c7d5515ed65b4631106fdb
dreamhead <dreamhead.cn@gmail.com>
2018-05-16 22:21:10 +0800

    added missing final to header detector

--- moco-core/src/main/java/com/github/dreamhead/moco/handler/HeaderDetector.java
+++ moco-core/src/main/java/com/github/dreamhead/moco/handler/HeaderDetector.java
@@ -7,11 +7,11 @@ import static com.github.dreamhead.moco.util.HttpHeaders.isForHeaderName;
 import static com.google.common.collect.FluentIterable.from;
 
 public class HeaderDetector {
-    public boolean hasContentType(final HttpResponse httpResponse) {
+    public final boolean hasContentType(final HttpResponse httpResponse) {
         return hasHeader(httpResponse, HttpHeaders.CONTENT_TYPE);
     }
 
-    public boolean hasHeader(final HttpResponse httpResponse, final String name) {
+    public final boolean hasHeader(final HttpResponse httpResponse, final String name) {
         return from(httpResponse.getHeaders().entrySet()).anyMatch(isForHeaderName(name));
     }
 }
