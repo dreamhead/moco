8da5d64441e0a17d8b5b34a2f72d6b47a4fac9a8
dreamhead <dreamhead.cn@gmail.com>
2018-05-23 22:19:12 +0800

    removed redundant final from file watcher factory

--- moco-runner/src/main/java/com/github/dreamhead/moco/runner/watcher/FileWatcherFactory.java
+++ moco-runner/src/main/java/com/github/dreamhead/moco/runner/watcher/FileWatcherFactory.java
@@ -5,5 +5,5 @@ import com.google.common.base.Function;
 import java.io.File;
 
 public interface FileWatcherFactory {
-    Watcher createWatcher(final Function<File, Void> listener, final File... files);
+    Watcher createWatcher(Function<File, Void> listener, File... files);
 }
