001f0b3cbb41e6e0a95ec4f679af9e1d1ce94156
dreamhead <dreamhead.cn@gmail.com>
2018-05-26 22:38:13 +0800

    removed redundant final in request hit

--- moco-core/src/main/java/com/github/dreamhead/moco/RequestHit.java
+++ moco-core/src/main/java/com/github/dreamhead/moco/RequestHit.java
@@ -1,7 +1,7 @@
 package com.github.dreamhead.moco;
 
 public interface RequestHit extends MocoMonitor {
-    void verify(final UnexpectedRequestMatcher matcher, final VerificationMode mode);
+    void verify(UnexpectedRequestMatcher matcher, VerificationMode mode);
 
-    void verify(final RequestMatcher matcher, final VerificationMode mode);
+    void verify(RequestMatcher matcher, VerificationMode mode);
 }
