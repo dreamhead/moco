e2c988b11fff246893aa95d41fe02e4e669d1080
dreamhead <dreamhead.cn@gmail.com>
2018-05-28 13:39:38 +0800

    removed redundant final in secure cookie attribute

--- moco-core/src/main/java/com/github/dreamhead/moco/cookie/SecureCookieAttribute.java
+++ moco-core/src/main/java/com/github/dreamhead/moco/cookie/SecureCookieAttribute.java
@@ -4,7 +4,7 @@ import io.netty.handler.codec.http.cookie.Cookie;
 
 public final class SecureCookieAttribute extends ActualCookieAttribute {
     @Override
-    public final void visit(final Cookie cookie) {
+    public void visit(final Cookie cookie) {
         cookie.setSecure(true);
     }
 }
