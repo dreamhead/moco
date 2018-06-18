83dff5d07f73fde54466fed6b9bad501cf658c51
dreamhead <dreamhead.cn@gmail.com>
2018-06-18 10:59:21 +0800

    caught exception for json in template

--- moco-core/src/main/java/com/github/dreamhead/moco/resource/reader/TemplateRequest.java
+++ moco-core/src/main/java/com/github/dreamhead/moco/resource/reader/TemplateRequest.java
@@ -87,6 +87,10 @@ public class TemplateRequest {
     }
 
     public Object getJson() {
-        return Jsons.toObject(this.request.getContent().toString(), Object.class);
+        try {
+            return Jsons.toObject(this.request.getContent().toString(), Object.class);
+        } catch (Exception e) {
+            throw new IllegalArgumentException("Json content is expected", e);
+        }
     }
 }
