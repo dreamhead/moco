5b4f3fdff9f315d029f70f2d3175144aafe881f5
dreamhead <dreamhead.cn@gmail.com>
2018-06-29 21:44:37 +0800

    removed redundant final from rest setting

--- moco-core/src/main/java/com/github/dreamhead/moco/RestSetting.java
+++ moco-core/src/main/java/com/github/dreamhead/moco/RestSetting.java
@@ -4,5 +4,5 @@ import com.google.common.base.Optional;
 
 public interface RestSetting {
     boolean isSimple();
-    Optional<ResponseHandler> getMatched(final RestIdMatcher resourceName, final HttpRequest httpRequest);
+    Optional<ResponseHandler> getMatched(RestIdMatcher resourceName, HttpRequest httpRequest);
 }
