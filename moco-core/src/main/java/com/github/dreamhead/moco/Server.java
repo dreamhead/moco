6131a07f1dc8e9000187dbb97686c03950631d91
dreamhead <dreamhead.cn@gmail.com>
2018-06-30 09:48:38 +0800

    removed redundant final from server

--- moco-core/src/main/java/com/github/dreamhead/moco/Server.java
+++ moco-core/src/main/java/com/github/dreamhead/moco/Server.java
@@ -3,7 +3,7 @@ package com.github.dreamhead.moco;
 public interface Server<T extends ResponseSetting> extends ResponseSetting<T> {
     int port();
 
-    T request(final RequestMatcher matcher);
+    T request(RequestMatcher matcher);
 
-    T request(final RequestMatcher... matchers);
+    T request(RequestMatcher... matchers);
 }
