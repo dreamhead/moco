16e383ba23bb395d899c2d75a7d650b2d9c98c67
dreamhead <dreamhead.cn@gmail.com>
2018-07-03 22:04:09 +0800

    removed redundant final from rest server

--- moco-core/src/main/java/com/github/dreamhead/moco/RestServer.java
+++ moco-core/src/main/java/com/github/dreamhead/moco/RestServer.java
@@ -1,5 +1,5 @@
 package com.github.dreamhead.moco;
 
 public interface RestServer extends HttpServer {
-    void resource(final String name, final RestSetting setting, final RestSetting... settings);
+    void resource(String name, RestSetting setting, RestSetting... settings);
 }
