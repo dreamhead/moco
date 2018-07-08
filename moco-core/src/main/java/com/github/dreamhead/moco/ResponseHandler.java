9449c631409a281bf1173e53c8df98ea5f3aad50
dreamhead <dreamhead.cn@gmail.com>
2018-07-08 08:42:38 +0800

    removed redundant final from response handler

--- moco-core/src/main/java/com/github/dreamhead/moco/ResponseHandler.java
+++ moco-core/src/main/java/com/github/dreamhead/moco/ResponseHandler.java
@@ -3,5 +3,5 @@ package com.github.dreamhead.moco;
 import com.github.dreamhead.moco.internal.SessionContext;
 
 public interface ResponseHandler extends ConfigApplier<ResponseHandler> {
-    void writeToResponse(final SessionContext context);
+    void writeToResponse(SessionContext context);
 }
