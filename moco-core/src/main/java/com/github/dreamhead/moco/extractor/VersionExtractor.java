746f0b174b47662b21bbdedf6e4e4a2b80d24de6
dreamhead <dreamhead.cn@gmail.com>
2018-07-29 22:17:31 +0800

    added missing final to version extractor

--- moco-core/src/main/java/com/github/dreamhead/moco/extractor/VersionExtractor.java
+++ moco-core/src/main/java/com/github/dreamhead/moco/extractor/VersionExtractor.java
@@ -8,7 +8,7 @@ import static com.google.common.base.Optional.of;
 
 public class VersionExtractor extends HttpRequestExtractor<String> {
     @Override
-    protected Optional<String> doExtract(final HttpRequest request) {
+    protected final Optional<String> doExtract(final HttpRequest request) {
         return of(request.getVersion().text());
     }
 }
