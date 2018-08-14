5287c28a903a556fe047e3fdbd9effd16cf617f7
dreamhead <dreamhead.cn@gmail.com>
2018-08-14 08:14:37 +0800

    removed redundant final from resource config applier

--- moco-core/src/main/java/com/github/dreamhead/moco/resource/ResourceConfigApplier.java
+++ moco-core/src/main/java/com/github/dreamhead/moco/resource/ResourceConfigApplier.java
@@ -3,5 +3,5 @@ package com.github.dreamhead.moco.resource;
 import com.github.dreamhead.moco.MocoConfig;
 
 public interface ResourceConfigApplier {
-    Resource apply(final MocoConfig config, final Resource resource);
+    Resource apply(MocoConfig config, Resource resource);
 }
