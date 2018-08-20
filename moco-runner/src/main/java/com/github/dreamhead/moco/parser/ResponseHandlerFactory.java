dcfc26866aa88e471b28eca8b59983433180fe6a
dreamhead <dreamhead.cn@gmail.com>
2018-08-20 21:16:52 +0800

    removed redundant final from response handler factory

--- moco-runner/src/main/java/com/github/dreamhead/moco/parser/ResponseHandlerFactory.java
+++ moco-runner/src/main/java/com/github/dreamhead/moco/parser/ResponseHandlerFactory.java
@@ -4,5 +4,5 @@ import com.github.dreamhead.moco.ResponseHandler;
 import com.github.dreamhead.moco.parser.model.ResponseSetting;
 
 public interface ResponseHandlerFactory {
-    ResponseHandler createResponseHandler(final ResponseSetting request);
+    ResponseHandler createResponseHandler(ResponseSetting request);
 }
