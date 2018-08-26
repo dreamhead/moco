00c5382cefa2fcc8a6a76fcad2c7e8552686d886
dreamhead <dreamhead.cn@gmail.com>
2018-08-26 17:04:29 +0800

    added missing final to http request extractor

--- moco-core/src/main/java/com/github/dreamhead/moco/HttpRequestExtractor.java
+++ moco-core/src/main/java/com/github/dreamhead/moco/HttpRequestExtractor.java
@@ -5,7 +5,7 @@ import com.google.common.base.Optional;
 public abstract class HttpRequestExtractor<T> implements RequestExtractor<T> {
     protected abstract Optional<T> doExtract(HttpRequest request);
 
-    public Optional<T> extract(final Request request) {
+    public final Optional<T> extract(final Request request) {
         if (HttpRequest.class.isInstance(request)) {
             return doExtract(HttpRequest.class.cast(request));
         }
