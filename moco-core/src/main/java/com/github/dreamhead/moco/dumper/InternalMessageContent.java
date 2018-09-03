e371ef55b61f34a29c4207c00c24e82cc7abb684
dreamhead <dreamhead.cn@gmail.com>
2018-09-03 22:40:02 +0800

    added missing final to internal message content

--- moco-core/src/main/java/com/github/dreamhead/moco/dumper/InternalMessageContent.java
+++ moco-core/src/main/java/com/github/dreamhead/moco/dumper/InternalMessageContent.java
@@ -9,7 +9,7 @@ import java.nio.charset.Charset;
 import static com.github.dreamhead.moco.model.MessageContent.content;
 
 @JsonAutoDetect(fieldVisibility = JsonAutoDetect.Visibility.ANY)
-public class InternalMessageContent {
+public final class InternalMessageContent {
     private byte[] content;
     private Charset charset;
 
