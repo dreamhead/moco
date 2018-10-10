13fabee8cbb608f948e33e2d0203ca6f846ed2f9
dreamhead <dreamhead.cn@gmail.com>
2018-10-11 06:13:32 +0800

    added missing final to rest head setting

--- moco-runner/src/main/java/com/github/dreamhead/moco/parser/model/RestHeadSetting.java
+++ moco-runner/src/main/java/com/github/dreamhead/moco/parser/model/RestHeadSetting.java
@@ -5,7 +5,7 @@ import com.github.dreamhead.moco.MocoRest;
 import com.github.dreamhead.moco.RestSettingBuilder;
 
 @JsonAutoDetect(fieldVisibility = JsonAutoDetect.Visibility.ANY)
-public class RestHeadSetting extends RestSingleSetting {
+public final class RestHeadSetting extends RestSingleSetting {
     @Override
     protected RestSettingBuilder doStartRestSetting() {
         if (hasId()) {
