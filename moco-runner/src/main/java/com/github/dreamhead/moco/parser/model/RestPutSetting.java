f89fca5eb6079ca44bf118fc4e41f8ff9580b7bd
dreamhead <dreamhead.cn@gmail.com>
2018-10-12 06:55:58 +0800

    added missing final to rest put setting

--- moco-runner/src/main/java/com/github/dreamhead/moco/parser/model/RestPutSetting.java
+++ moco-runner/src/main/java/com/github/dreamhead/moco/parser/model/RestPutSetting.java
@@ -5,7 +5,7 @@ import com.github.dreamhead.moco.MocoRest;
 import com.github.dreamhead.moco.RestSettingBuilder;
 
 @JsonAutoDetect(fieldVisibility = JsonAutoDetect.Visibility.ANY)
-public class RestPutSetting extends RestSingleSetting {
+public final class RestPutSetting extends RestSingleSetting {
     @Override
     protected RestSettingBuilder doStartRestSetting() {
         return MocoRest.put(this.id());
