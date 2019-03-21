10893a28ac0c2495378c98e6bcb66d0216fa3348
dreamhead <dreamhead.cn@gmail.com>
2019-03-21 21:29:23 +0800

    added missing final to actual http server

--- moco-core/src/main/java/com/github/dreamhead/moco/internal/ActualHttpServer.java
+++ moco-core/src/main/java/com/github/dreamhead/moco/internal/ActualHttpServer.java
@@ -41,7 +41,7 @@ public class ActualHttpServer extends HttpConfiguration<ActualHttpServer> {
         return absent();
     }
 
-    private SslHandler asSslHandler(HttpsCertificate certificate) {
+    private SslHandler asSslHandler(final HttpsCertificate certificate) {
         SSLEngine sslEngine = certificate.createSSLEngine();
         sslEngine.setUseClientMode(false);
         return new SslHandler(sslEngine);
