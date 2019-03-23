a78830d657d2141404f20843bd3ed0799afec772
dreamhead <dreamhead.cn@gmail.com>
2019-03-23 22:11:24 +0800

    removed unused import in api utils

--- moco-core/src/main/java/com/github/dreamhead/moco/internal/ApiUtils.java
+++ moco-core/src/main/java/com/github/dreamhead/moco/internal/ApiUtils.java
@@ -24,7 +24,6 @@ import com.github.dreamhead.moco.resource.Resource;
 import com.github.dreamhead.moco.resource.reader.ExtractorVariable;
 import com.github.dreamhead.moco.resource.reader.Variable;
 import com.google.common.base.Function;
-import com.google.common.base.Optional;
 import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.Maps;
 
