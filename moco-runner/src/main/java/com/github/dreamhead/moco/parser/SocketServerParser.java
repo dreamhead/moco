82822816f4b17abb01587aa2628be8ebd716b46c
dreamhead <dreamhead.cn@gmail.com>
2019-04-15 21:57:48 +0800

    removed unused import in socket server parser

--- moco-runner/src/main/java/com/github/dreamhead/moco/parser/SocketServerParser.java
+++ moco-runner/src/main/java/com/github/dreamhead/moco/parser/SocketServerParser.java
@@ -4,7 +4,6 @@ import com.github.dreamhead.moco.MocoConfig;
 import com.github.dreamhead.moco.SocketServer;
 import com.github.dreamhead.moco.internal.ActualSocketServer;
 import com.github.dreamhead.moco.parser.model.SessionSetting;
-import com.google.common.base.Optional;
 import com.google.common.collect.ImmutableList;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
