316f447277e75c7114c7bbcce8241dfeab2874d5
dreamhead <dreamhead.cn@gmail.com>
2019-06-04 21:54:51 +0800

    added response with http header

--- moco-core/src/main/java/com/github/dreamhead/moco/ResponseBase.java
+++ moco-core/src/main/java/com/github/dreamhead/moco/ResponseBase.java
@@ -7,4 +7,5 @@ public interface ResponseBase<T> {
     T response(String content);
     T response(Resource resource);
     T response(MocoProcedure procedure);
+    T response(HttpHeader header);
 }
--- moco-core/src/main/java/com/github/dreamhead/moco/internal/AbstractResponseBase.java
+++ moco-core/src/main/java/com/github/dreamhead/moco/internal/AbstractResponseBase.java
@@ -1,5 +1,6 @@
 package com.github.dreamhead.moco.internal;
 
+import com.github.dreamhead.moco.HttpHeader;
 import com.github.dreamhead.moco.MocoProcedure;
 import com.github.dreamhead.moco.ResponseBase;
 import com.github.dreamhead.moco.resource.Resource;
@@ -23,5 +24,10 @@ public abstract class AbstractResponseBase<T> implements ResponseBase<T> {
     @Override
     public final T response(final MocoProcedure procedure) {
         return this.response(with(checkNotNull(procedure, "Procedure should not be null")));
+    }
+
+    @Override
+    public final T response(final HttpHeader header) {
+        return this.response(with(checkNotNull(header, "Procedure should not be null")));
     }
 }
