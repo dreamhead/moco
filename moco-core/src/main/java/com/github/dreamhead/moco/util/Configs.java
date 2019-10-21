5f0cf75a522ef5f92457b4c25e21e127bd004829
dreamhead <dreamhead.cn@gmail.com>
2019-10-21 21:57:23 +0800

    migrated guava function to java 8 function in configs

--- moco-core/src/main/java/com/github/dreamhead/moco/util/Configs.java
+++ moco-core/src/main/java/com/github/dreamhead/moco/util/Configs.java
@@ -2,12 +2,10 @@ package com.github.dreamhead.moco.util;
 
 import com.github.dreamhead.moco.ConfigApplier;
 import com.github.dreamhead.moco.MocoConfig;
-import com.google.common.base.Function;
 import com.google.common.collect.ImmutableList;
 
 import java.util.List;
-
-import static com.google.common.collect.FluentIterable.from;
+import java.util.stream.Collectors;
 
 public final class Configs {
     public static <T extends ConfigApplier<T>> T configItem(final T source, final MocoConfig... configs) {
@@ -24,16 +22,9 @@ public final class Configs {
 
     public static <T extends ConfigApplier<T>> ImmutableList<T> configItems(final List<T> items,
                                                                             final MocoConfig... configs) {
-        return from(items).transform(Configs.<T>config(configs)).toList();
-    }
-
-    private static <T extends ConfigApplier<T>> Function<T, T> config(final MocoConfig... configs) {
-        return new Function<T, T>() {
-            @Override
-            public T apply(final T item) {
-                return configItem(item, configs);
-            }
-        };
+        return ImmutableList.copyOf(items.stream()
+                .map(item -> configItem(item, configs))
+                .collect(Collectors.toList()));
     }
 
     private Configs() {
