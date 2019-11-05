73aa0a6dd38a0d3d8d3c134d333dd9b47eade8b7
dreamhead <dreamhead.cn@gmail.com>
2019-11-05 22:26:10 +0800

    migrated guava function and predicate to java 8 style in setting runner

--- moco-runner/src/main/java/com/github/dreamhead/moco/runner/SettingRunner.java
+++ moco-runner/src/main/java/com/github/dreamhead/moco/runner/SettingRunner.java
@@ -4,19 +4,21 @@ import com.github.dreamhead.moco.MocoException;
 import com.github.dreamhead.moco.bootstrap.arg.StartArgs;
 import com.github.dreamhead.moco.parser.GlobalSettingParser;
 import com.github.dreamhead.moco.parser.model.GlobalSetting;
-import com.google.common.base.Function;
-import com.google.common.base.Predicate;
-import com.google.common.collect.FluentIterable;
 import com.google.common.collect.ImmutableList;
 
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.InputStream;
+import java.util.List;
+import java.util.function.Function;
+import java.util.function.Predicate;
+import java.util.stream.Stream;
 
 import static com.github.dreamhead.moco.runner.JsonRunner.newJsonRunnerWithSetting;
 import static com.github.dreamhead.moco.runner.RunnerSetting.aRunnerSetting;
-import static com.google.common.collect.FluentIterable.from;
+import static com.google.common.collect.ImmutableList.toImmutableList;
+import static java.util.stream.Collectors.toList;
 
 public final class SettingRunner implements Runner {
     private final GlobalSettingParser parser = new GlobalSettingParser();
@@ -24,12 +26,14 @@ public final class SettingRunner implements Runner {
     private final String env;
     private final StartArgs startArgs;
     private Runner runner;
-    private final FluentIterable<File> files;
+    private final List<File> files;
 
     public SettingRunner(final InputStream stream, final StartArgs args) {
         this.env = args.getEnv().orElse(null);
         this.globalSettings = parser.parse(stream);
-        this.files = from(globalSettings).transformAndConcat(toFiles());
+        this.files = globalSettings.stream()
+                .flatMap(toFiles())
+                .collect(toList());
         this.startArgs = args;
     }
 
@@ -38,46 +42,36 @@ public final class SettingRunner implements Runner {
     }
 
     public void run() {
-        runner = newJsonRunnerWithSetting(from(globalSettings)
-                .filter(byEnv(this.env))
-                .transform(toRunnerSetting()), startArgs);
+        runner = newJsonRunnerWithSetting(
+                globalSettings.stream()
+                        .filter(byEnv(this.env))
+                        .map(toRunnerSetting())
+                        .collect(toList()), startArgs);
         runner.run();
     }
 
     private Predicate<? super GlobalSetting> byEnv(final String env) {
-        return new Predicate<GlobalSetting>() {
-            @Override
-            public boolean apply(final GlobalSetting globalSetting) {
-                return env == null || env.equalsIgnoreCase(globalSetting.getEnv());
-
-            }
-        };
+        return globalSetting -> env == null || env.equalsIgnoreCase(globalSetting.getEnv());
     }
 
     private Function<GlobalSetting, RunnerSetting> toRunnerSetting() {
-        return new Function<GlobalSetting, RunnerSetting>() {
-            @Override
-            public RunnerSetting apply(final GlobalSetting setting) {
-                return aRunnerSetting()
-                        .addStreams(from(setting.includes()).transform(toStream()).toList())
-                        .withContext(setting.getContext())
-                        .withFileRoot(setting.getFileRoot())
-                        .withRequest(setting.getRequest())
-                        .withResponse(setting.getResponse())
-                        .build();
-            }
-        };
+        return setting -> aRunnerSetting()
+                .addStreams(setting.includes().stream()
+                        .map(toStream())
+                        .collect(toImmutableList()))
+                .withContext(setting.getContext())
+                .withFileRoot(setting.getFileRoot())
+                .withRequest(setting.getRequest())
+                .withResponse(setting.getResponse())
+                .build();
     }
 
     private Function<String, InputStream> toStream() {
-        return new Function<String, InputStream>() {
-            @Override
-            public InputStream apply(final String input) {
-                try {
-                    return new FileInputStream(input);
-                } catch (FileNotFoundException e) {
-                    throw new MocoException(e);
-                }
+        return input -> {
+            try {
+                return new FileInputStream(input);
+            } catch (FileNotFoundException e) {
+                throw new MocoException(e);
             }
         };
     }
@@ -86,21 +80,7 @@ public final class SettingRunner implements Runner {
         runner.stop();
     }
 
-    private Function<? super GlobalSetting, Iterable<? extends File>> toFiles() {
-        return new Function<GlobalSetting, Iterable<? extends File>>() {
-            @Override
-            public Iterable<? extends File> apply(final GlobalSetting input) {
-                return from(input.includes()).transform(toFile());
-            }
-        };
-    }
-
-    private Function<String, File> toFile() {
-        return new Function<String, File>() {
-            @Override
-            public File apply(final String input) {
-                return new File(input);
-            }
-        };
+    private Function<? super GlobalSetting, Stream<? extends File>> toFiles() {
+        return input -> input.includes().stream().map(File::new);
     }
 }
