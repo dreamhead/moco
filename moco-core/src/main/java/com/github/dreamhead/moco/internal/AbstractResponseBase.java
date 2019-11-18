cfcad0d067d56ea702ad18793b352a6e7493a075
dreamhead <dreamhead.cn@gmail.com>
2019-11-18 22:33:19 +0800

    migrated guava function to java 8 style in abstract response base

--- moco-core/src/main/java/com/github/dreamhead/moco/internal/AbstractResponseBase.java
+++ moco-core/src/main/java/com/github/dreamhead/moco/internal/AbstractResponseBase.java
@@ -1,16 +1,17 @@
 package com.github.dreamhead.moco.internal;
 
+import com.github.dreamhead.moco.Moco;
 import com.github.dreamhead.moco.ResponseBase;
 import com.github.dreamhead.moco.ResponseElement;
 import com.github.dreamhead.moco.ResponseHandler;
 import com.github.dreamhead.moco.resource.Resource;
-import com.google.common.base.Function;
+
+import java.util.Arrays;
 
 import static com.github.dreamhead.moco.Moco.text;
 import static com.github.dreamhead.moco.Moco.with;
 import static com.github.dreamhead.moco.util.Preconditions.checkNotNullOrEmpty;
 import static com.google.common.base.Preconditions.checkNotNull;
-import static com.google.common.collect.FluentIterable.from;
 
 public abstract class AbstractResponseBase<T> implements ResponseBase<T> {
     @Override
@@ -21,22 +22,13 @@ public abstract class AbstractResponseBase<T> implements ResponseBase<T> {
     @Override
     public final T response(final ResponseElement element, final ResponseElement... elements) {
         return this.response(with(checkNotNull(element, "Response element should not be null")),
-                from(checkNotNull(elements, "Response elements should not be null"))
-                        .transform(elementAsResponseHandler())
-                        .toArray(ResponseHandler.class));
+                Arrays.stream(checkNotNull(elements, "Response elements should not be null"))
+                        .map(Moco::with)
+                        .toArray(ResponseHandler[]::new));
     }
 
     @Override
     public final T response(final Resource resource) {
         return this.response(with(checkNotNull(resource, "Resource should not be null")));
-    }
-
-    private Function<ResponseElement, ResponseHandler> elementAsResponseHandler() {
-        return new Function<ResponseElement, ResponseHandler>() {
-            @Override
-            public ResponseHandler apply(final ResponseElement input) {
-                return with(input);
-            }
-        };
     }
 }
