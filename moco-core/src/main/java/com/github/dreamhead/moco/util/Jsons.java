c307c44d14388ee73c2b9cd6bbb7e702e61433c0
dreamhead <dreamhead.cn@gmail.com>
2019-11-21 22:20:54 +0800

    migrated guava function to java 8 style in jsons

--- moco-core/src/main/java/com/github/dreamhead/moco/util/Jsons.java
+++ moco-core/src/main/java/com/github/dreamhead/moco/util/Jsons.java
@@ -7,8 +7,6 @@ import com.fasterxml.jackson.databind.exc.UnrecognizedPropertyException;
 import com.fasterxml.jackson.databind.type.CollectionType;
 import com.fasterxml.jackson.databind.type.TypeFactory;
 import com.github.dreamhead.moco.MocoException;
-import com.google.common.base.Function;
-import com.google.common.collect.FluentIterable;
 import com.google.common.collect.ImmutableList;
 import com.google.common.io.CharStreams;
 import org.slf4j.Logger;
@@ -20,8 +18,11 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.List;
 import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Stream;
 
 import static com.google.common.collect.ImmutableList.of;
+import static com.google.common.collect.ImmutableList.toImmutableList;
 import static java.lang.String.format;
 
 public final class Jsons {
@@ -73,25 +74,24 @@ public final class Jsons {
     public static <T> ImmutableList<T> toObjects(final ImmutableList<InputStream> streams,
                                                  final Class<T> elementClass) {
         final CollectionType type = DEFAULT_FACTORY.constructCollectionType(List.class, elementClass);
-        return FluentIterable.from(streams).transformAndConcat(Jsons.<T>toObject(type)).toList();
+        return streams.stream()
+                .flatMap(Jsons.<T>toObject(type))
+                .collect(toImmutableList());
     }
 
-    private static <T> Function<InputStream, Iterable<T>> toObject(final CollectionType type) {
-        return new Function<InputStream, Iterable<T>>() {
-            @Override
-            public Iterable<T> apply(final InputStream input) {
-                try (InputStream actual = input) {
-                    String text = CharStreams.toString(new InputStreamReader(actual));
-                    return DEFAULT_MAPPER.readValue(text, type);
-                } catch (UnrecognizedPropertyException e) {
-                    logger.info("Unrecognized field: {}", e.getMessage());
-                    throw new MocoException(format("Unrecognized field [ %s ], please check!", e.getPropertyName()));
-                } catch (JsonMappingException e) {
-                    logger.info("{} {}", e.getMessage(), e.getPathReference());
-                    throw new MocoException(e);
-                } catch (IOException e) {
-                    throw new MocoException(e);
-                }
+    private static <T> Function<InputStream, Stream<T>> toObject(final CollectionType type) {
+        return input -> {
+            try (InputStream actual = input) {
+                String text = CharStreams.toString(new InputStreamReader(actual));
+                return DEFAULT_MAPPER.<List<T>>readValue(text, type).stream();
+            } catch (UnrecognizedPropertyException e) {
+                logger.info("Unrecognized field: {}", e.getMessage());
+                throw new MocoException(format("Unrecognized field [ %s ], please check!", e.getPropertyName()));
+            } catch (JsonMappingException e) {
+                logger.info("{} {}", e.getMessage(), e.getPathReference());
+                throw new MocoException(e);
+            } catch (IOException e) {
+                throw new MocoException(e);
             }
         };
     }
