e0e88b3d6f76bb9bda2ac47f86fc1ab37940e931
dreamhead <dreamhead.cn@gmail.com>
2019-12-08 23:11:29 +0800

    extracted get recorded request in replay handler

--- moco-core/src/main/java/com/github/dreamhead/moco/recorder/DynamicReplayHandler.java
+++ moco-core/src/main/java/com/github/dreamhead/moco/recorder/DynamicReplayHandler.java
@@ -5,6 +5,7 @@ import com.github.dreamhead.moco.model.MessageContent;
 import com.github.dreamhead.moco.mount.AbstractHttpContentResponseHandler;
 import com.github.dreamhead.moco.resource.ContentResource;
 import com.google.common.net.MediaType;
+import org.apache.http.HttpHeaders;
 
 public class DynamicReplayHandler extends AbstractHttpContentResponseHandler implements ReplayHandler {
     private RecorderRegistry registry;
@@ -21,18 +22,35 @@ public class DynamicReplayHandler extends AbstractHttpContentResponseHandler imp
 
     @Override
     protected MessageContent responseContent(final HttpRequest request) {
-        String name = this.name.readFor(request).toString();
-        RequestRecorder recorder = registry.recorderOf(name);
-        HttpRequest recordedRequest = recorder.getRequest();
+        HttpRequest recordedRequest = getRecordedRequest(request);
         if (recordedRequest == null) {
             throw new IllegalArgumentException("No recorded request for [" + name + "]");
         }
+
         return replayModifier.readFor(recordedRequest);
+    }
+
+    private HttpRequest getRecordedRequest(final HttpRequest request) {
+        String name = this.name.readFor(request).toString();
+        RequestRecorder recorder = registry.recorderOf(name);
+        return recorder.getRequest();
     }
 
     @Override
     protected MediaType getContentType(final HttpRequest request) {
-        RequestRecorder recorder = registry.recorderOf(this.name.readFor(request).toString());
-        return recorder.getContentType();
+        HttpRequest recordedRequest = getRecordedRequest(request);
+        if (recordedRequest == null) {
+            return MediaType.PLAIN_TEXT_UTF_8;
+        }
+
+        String header = recordedRequest.getHeader(HttpHeaders.CONTENT_TYPE);
+        if (header == null) {
+            return MediaType.PLAIN_TEXT_UTF_8;
+        }
+        try {
+            return MediaType.parse(header);
+        } catch (Exception e) {
+            return MediaType.PLAIN_TEXT_UTF_8;
+        }
     }
 }
