c486c00fa4535ac4d205db55c58e37d2b2d6a281
dreamhead <dreamhead.cn@gmail.com>
2020-01-02 21:56:05 +0800

    added argument check for moco recorder configuration

--- moco-core/src/main/java/com/github/dreamhead/moco/MocoRecorders.java
+++ moco-core/src/main/java/com/github/dreamhead/moco/MocoRecorders.java
@@ -13,6 +13,7 @@ import com.github.dreamhead.moco.recorder.StaticRecordHandler;
 import com.github.dreamhead.moco.recorder.StaticReplayHandler;
 
 import static com.github.dreamhead.moco.Moco.template;
+import static com.github.dreamhead.moco.util.Preconditions.checkNotNullOrEmpty;
 
 public final class MocoRecorders {
     public static ResponseHandler record(final RequestRecorder recorder) {
@@ -33,19 +34,19 @@ public final class MocoRecorders {
     }
 
     public static RecorderGroup group(final String name) {
-        return new RecorderGroup(name);
+        return new RecorderGroup(checkNotNullOrEmpty(name, "Identifier should not be empty"));
     }
 
     public static RecorderTape tape(final String path) {
-        return new RecorderTape(path);
+        return new RecorderTape(checkNotNullOrEmpty(path, "Identifier should not be empty"));
     }
 
     public static RecorderIdentifier identifier(final String text) {
-        return new RecorderIdentifier(template(text));
+        return new RecorderIdentifier(template(checkNotNullOrEmpty(text, "Identifier should not be empty")));
     }
 
     public static RecorderModifier modifier(final String text) {
-        return new RecorderModifier(template(text));
+        return new RecorderModifier(template(checkNotNullOrEmpty(text, "Identifier should not be empty")));
     }
 
     private MocoRecorders() {
