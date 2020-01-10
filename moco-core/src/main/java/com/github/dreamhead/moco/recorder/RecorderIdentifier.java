a73518d53ccd513fa53a60f8778183547b776397
dreamhead <dreamhead.cn@gmail.com>
2020-01-10 22:05:29 +0800

    added missing final to recorder identifier

--- moco-core/src/main/java/com/github/dreamhead/moco/recorder/RecorderIdentifier.java
+++ moco-core/src/main/java/com/github/dreamhead/moco/recorder/RecorderIdentifier.java
@@ -10,12 +10,12 @@ public class RecorderIdentifier implements RecorderConfig {
         this.resource = resource;
     }
 
-    public String getIdentifier(final HttpRequest httpRequest) {
+    public final String getIdentifier(final HttpRequest httpRequest) {
         return this.resource.readFor(httpRequest).toString();
     }
 
     @Override
-    public boolean isFor(final String name) {
+    public final boolean isFor(final String name) {
         return IDENTIFIER.equalsIgnoreCase(name);
     }
 }
