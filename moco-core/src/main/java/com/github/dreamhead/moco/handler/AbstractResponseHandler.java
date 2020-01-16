ce1cc2e30b97a814cefa7034e01728cc98a7e716
dreamhead <dreamhead.cn@gmail.com>
2020-01-16 22:21:37 +0800

    made doApply protected in abstract response handler

--- moco-core/src/main/java/com/github/dreamhead/moco/handler/AbstractResponseHandler.java
+++ moco-core/src/main/java/com/github/dreamhead/moco/handler/AbstractResponseHandler.java
@@ -4,7 +4,7 @@ import com.github.dreamhead.moco.MocoConfig;
 import com.github.dreamhead.moco.ResponseHandler;
 
 public abstract class AbstractResponseHandler implements ResponseHandler {
-    public ResponseHandler doApply(final MocoConfig config) {
+    protected ResponseHandler doApply(final MocoConfig config) {
         return this;
     }
 
