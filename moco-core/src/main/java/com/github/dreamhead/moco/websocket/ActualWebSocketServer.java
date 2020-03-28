4831f818d17db3bea6c8f9310cc4e78012415692
dreamhead <dreamhead.cn@gmail.com>
2020-03-28 17:18:06 +0800

    introduced websocket response setting

--- moco-core/src/main/java/com/github/dreamhead/moco/websocket/ActualWebSocketServer.java
+++ moco-core/src/main/java/com/github/dreamhead/moco/websocket/ActualWebSocketServer.java
@@ -27,7 +27,7 @@ public class ActualWebSocketServer implements WebSocketServer {
         this.connected = resource;
     }
 
-    private void addChannel(final Channel channel) {
+    private void connect(final Channel channel) {
         this.group.add(channel);
     }
 
@@ -57,7 +57,7 @@ public class ActualWebSocketServer implements WebSocketServer {
         }
 
         handshaker.handshake(channel, request);
-        addChannel(channel);
+        connect(channel);
         sendConnected(channel);
     }
 }
---
+++ moco-core/src/main/java/com/github/dreamhead/moco/websocket/WebsocketResponseSetting.java
@@ -0,0 +1,7 @@
+package com.github.dreamhead.moco.websocket;
+
+import com.github.dreamhead.moco.ResponseSetting;
+
+public interface WebsocketResponseSetting extends ResponseSetting<WebsocketResponseSetting> {
+
+}
