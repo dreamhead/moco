270fea7a5c818b1ef073067484d1f3a2153a2373
dreamhead <dreamhead.cn@gmail.com>
2020-03-29 21:26:18 +0800

    introduced websocket setting

---
+++ moco-core/src/main/java/com/github/dreamhead/moco/websocket/WebsocketSetting.java
@@ -0,0 +1,33 @@
+package com.github.dreamhead.moco.websocket;
+
+import com.github.dreamhead.moco.MocoConfig;
+import com.github.dreamhead.moco.RequestMatcher;
+import com.github.dreamhead.moco.internal.InternalApis;
+import com.github.dreamhead.moco.matcher.AndRequestMatcher;
+import com.github.dreamhead.moco.setting.BaseSetting;
+import com.github.dreamhead.moco.setting.Setting;
+
+import static com.github.dreamhead.moco.util.Configs.configItem;
+import static com.google.common.collect.ImmutableList.of;
+
+public class WebsocketSetting extends BaseSetting<WebsocketResponseSetting>
+        implements Setting<WebsocketResponseSetting> {
+    protected WebsocketSetting(final RequestMatcher matcher) {
+        super(matcher);
+    }
+
+    @Override
+    protected BaseSetting<WebsocketResponseSetting> createSetting(final RequestMatcher matcher) {
+        return new WebsocketSetting(matcher);
+    }
+
+    @Override
+    protected RequestMatcher configMatcher(final RequestMatcher matcher, final MocoConfig config) {
+        RequestMatcher appliedMatcher = configItem(matcher, config);
+        if (config.isFor(MocoConfig.URI_ID) && matcher == appliedMatcher) {
+            return new AndRequestMatcher(of(appliedMatcher, InternalApis.context((String) config.apply(""))));
+        }
+
+        return appliedMatcher;
+    }
+}
