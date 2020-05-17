1ddefcff3e85fd2d0e7a6f3ad27c6c8a26287162
dreamhead <dreamhead.cn@gmail.com>
2020-05-17 21:34:34 +0800

    applied optional map in form request extractor

--- moco-core/src/main/java/com/github/dreamhead/moco/extractor/FormRequestExtractor.java
+++ moco-core/src/main/java/com/github/dreamhead/moco/extractor/FormRequestExtractor.java
@@ -6,8 +6,6 @@ import com.google.common.collect.ImmutableMap;
 
 import java.util.Optional;
 
-import static java.util.Optional.empty;
-
 public final class FormRequestExtractor extends HttpRequestExtractor<String> {
     private final FormsRequestExtractor extractor = new FormsRequestExtractor();
     private final String key;
@@ -19,10 +17,6 @@ public final class FormRequestExtractor extends HttpRequestExtractor<String> {
     @Override
     protected Optional<String> doExtract(final HttpRequest request) {
         Optional<ImmutableMap<String, String>> forms = extractor.extract(request);
-        if (forms.isPresent()) {
-            return Optional.ofNullable(forms.get().get(key));
-        }
-
-        return empty();
+        return forms.map(formValues -> formValues.get(key));
     }
 }
