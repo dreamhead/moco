e3a36d193b7cf8b417a963626cad8fcb317497c1
dreamhead <dreamhead.cn@gmail.com>
2020-05-22 17:02:58 +0800

    added missing final to suppliers

--- moco-core/src/main/java/com/github/dreamhead/moco/util/Suppliers.java
+++ moco-core/src/main/java/com/github/dreamhead/moco/util/Suppliers.java
@@ -5,14 +5,18 @@ import java.util.concurrent.atomic.AtomicReference;
 import java.util.function.Function;
 import java.util.function.Supplier;
 
-public class Suppliers {
+public final class Suppliers {
     public static <T> Supplier<T> memoize(final Supplier<T> delegate) {
         AtomicReference<T> value = new AtomicReference<>();
         return () -> {
             T val = value.get();
             if (val == null) {
-                val = value.updateAndGet(cur -> cur == null ?
-                        Objects.requireNonNull(delegate.get()) : cur);
+                val = value.updateAndGet(cur -> {
+                    if (cur == null) {
+                        return Objects.requireNonNull(delegate.get());
+                    }
+                    return cur;
+                });
             }
             return val;
         };
