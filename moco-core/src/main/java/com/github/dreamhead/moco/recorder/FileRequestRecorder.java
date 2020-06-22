09f62f0c39e2017210a661e9cdd21d95def54652
dreamhead <dreamhead.cn@gmail.com>
2020-06-22 18:41:06 +0800

    added missing final to fields in file request recorder

--- moco-core/src/main/java/com/github/dreamhead/moco/recorder/FileRequestRecorder.java
+++ moco-core/src/main/java/com/github/dreamhead/moco/recorder/FileRequestRecorder.java
@@ -3,8 +3,8 @@ package com.github.dreamhead.moco.recorder;
 import com.github.dreamhead.moco.HttpRequest;
 
 public class FileRequestRecorder implements RequestRecorder {
-    private String name;
-    private RecorderTape tape;
+    private final String name;
+    private final RecorderTape tape;
 
     public FileRequestRecorder(final String name, final RecorderTape tape) {
         this.name = name;
