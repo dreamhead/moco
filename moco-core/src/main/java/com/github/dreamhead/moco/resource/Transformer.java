5eea8134fb4f2a9c1d3bedd45a2cf9ff755b9ae6
dreamhead <dreamhead.cn@gmail.com>
2020-07-05 22:21:08 +0800

    removed redundant final from transform in Transformer

--- moco-core/src/main/java/com/github/dreamhead/moco/resource/Transformer.java
+++ moco-core/src/main/java/com/github/dreamhead/moco/resource/Transformer.java
@@ -3,5 +3,5 @@ package com.github.dreamhead.moco.resource;
 import java.util.function.Function;
 
 public interface Transformer<T, U> {
-    T transform(final Function<U, U> transformer);
+    T transform(Function<U, U> transformer);
 }
