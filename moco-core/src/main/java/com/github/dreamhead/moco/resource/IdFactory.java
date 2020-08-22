f859e23899a5d59bb8337e56b1766c04c4063e72
dreamhead <dreamhead.cn@gmail.com>
2020-08-22 19:18:48 +0800

    replaced id implementation with lambda in id factory

--- moco-core/src/main/java/com/github/dreamhead/moco/resource/IdFactory.java
+++ moco-core/src/main/java/com/github/dreamhead/moco/resource/IdFactory.java
@@ -2,12 +2,7 @@ package com.github.dreamhead.moco.resource;
 
 public final class IdFactory {
     public static Identifiable id(final String id) {
-        return new Identifiable() {
-            @Override
-            public String id() {
-                return id;
-            }
-        };
+        return () -> id;
     }
 
     private IdFactory() {
