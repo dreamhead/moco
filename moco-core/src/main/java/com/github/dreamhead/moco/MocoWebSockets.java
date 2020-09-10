1f47ebe5c18b1b3d91b4951fd470c4edda1d0f53
dreamhead <dreamhead.cn@gmail.com>
2020-09-10 22:49:37 +0800

    checked broadcast arguments

--- moco-core/src/main/java/com/github/dreamhead/moco/MocoWebSockets.java
+++ moco-core/src/main/java/com/github/dreamhead/moco/MocoWebSockets.java
@@ -2,8 +2,10 @@ package com.github.dreamhead.moco;
 
 import com.github.dreamhead.moco.websocket.WebSocketBroadcastHandler;
 
+import static com.github.dreamhead.moco.util.Preconditions.checkNotNullOrEmpty;
+
 public class MocoWebSockets {
     public static ResponseHandler broadcast(final String content) {
-        return new WebSocketBroadcastHandler(content);
+        return new WebSocketBroadcastHandler(checkNotNullOrEmpty(content, "Content should not be null"));
     }
 }
