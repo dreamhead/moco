66b4ffbf558dd41e5da28e2c15afbbd97c4f5deb
dreamhead <dreamhead.cn@gmail.com>
2020-09-18 21:42:31 +0800

    made websocket broadcast handler extend abstract response handler

--- moco-core/src/main/java/com/github/dreamhead/moco/websocket/WebSocketBroadcastHandler.java
+++ moco-core/src/main/java/com/github/dreamhead/moco/websocket/WebSocketBroadcastHandler.java
@@ -2,6 +2,7 @@ package com.github.dreamhead.moco.websocket;
 
 import com.github.dreamhead.moco.MocoConfig;
 import com.github.dreamhead.moco.ResponseHandler;
+import com.github.dreamhead.moco.handler.AbstractResponseHandler;
 import com.github.dreamhead.moco.internal.SessionContext;
 import com.github.dreamhead.moco.model.MessageContent;
 import com.github.dreamhead.moco.resource.Resource;
@@ -9,7 +10,7 @@ import com.github.dreamhead.moco.util.ByteBufs;
 import io.netty.buffer.ByteBuf;
 import io.netty.handler.codec.http.websocketx.BinaryWebSocketFrame;
 
-public class WebSocketBroadcastHandler implements ResponseHandler {
+public class WebSocketBroadcastHandler extends AbstractResponseHandler {
     private final Resource content;
 
     public WebSocketBroadcastHandler(final Resource content) {
@@ -24,7 +25,7 @@ public class WebSocketBroadcastHandler implements ResponseHandler {
     }
 
     @Override
-    public ResponseHandler apply(final MocoConfig config) {
+    public ResponseHandler doApply(final MocoConfig config) {
         Resource applied = content.apply(config);
         if (applied == content) {
             return this;
