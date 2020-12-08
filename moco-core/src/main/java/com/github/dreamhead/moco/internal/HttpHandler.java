a4430ab4a573bbba8e3d707db356fe257ecfd8c1
dreamhead <dreamhead.cn@gmail.com>
2020-12-08 22:32:37 +0800

    introduced http handler

---
+++ moco-core/src/main/java/com/github/dreamhead/moco/internal/HttpHandler.java
@@ -0,0 +1,62 @@
+package com.github.dreamhead.moco.internal;
+
+import com.github.dreamhead.moco.HttpRequest;
+import com.github.dreamhead.moco.model.DefaultHttpRequest;
+import com.github.dreamhead.moco.model.DefaultMutableHttpResponse;
+import io.netty.handler.codec.http.FullHttpRequest;
+import io.netty.handler.codec.http.FullHttpResponse;
+import io.netty.handler.codec.http.HttpResponseStatus;
+
+import static com.github.dreamhead.moco.model.DefaultMutableHttpResponse.newResponse;
+import static io.netty.handler.codec.http.HttpUtil.isContentLengthSet;
+import static io.netty.handler.codec.http.HttpUtil.isKeepAlive;
+import static io.netty.handler.codec.http.HttpUtil.setContentLength;
+import static io.netty.handler.codec.http.HttpUtil.setKeepAlive;
+
+public class HttpHandler {
+    private static final int DEFAULT_STATUS = HttpResponseStatus.OK.code();
+
+    private final ActualHttpServer server;
+
+    public HttpHandler(final ActualHttpServer server) {
+        this.server = server;
+    }
+
+    public FullHttpResponse handleRequest(final FullHttpRequest message, MocoHandler mocoHandler) {
+        HttpRequest request = DefaultHttpRequest.newRequest(message);
+        DefaultMutableHttpResponse httpResponse = getHttpResponse(request, mocoHandler);
+        FullHttpResponse response = httpResponse.toFullResponse();
+        prepareForKeepAlive(message, response, mocoHandler);
+        return response;
+    }
+
+    private DefaultMutableHttpResponse getHttpResponse(final HttpRequest request, MocoHandler mocoHandler) {
+        DefaultMutableHttpResponse httpResponse = newResponse(request, DEFAULT_STATUS);
+        SessionContext context = new SessionContext(request, httpResponse);
+        return doGetResponse(request, context);
+    }
+
+    private DefaultMutableHttpResponse doGetResponse(final HttpRequest request, final SessionContext context) {
+        try {
+            return (DefaultMutableHttpResponse) server.getResponse(context)
+                    .orElse(newResponse(request, HttpResponseStatus.BAD_REQUEST.code()));
+        } catch (RuntimeException e) {
+            return newResponse(request, HttpResponseStatus.BAD_REQUEST.code());
+        } catch (Exception e) {
+            return newResponse(request, HttpResponseStatus.INTERNAL_SERVER_ERROR.code());
+        }
+    }
+
+    private void setContentLengthForKeepAlive(final FullHttpResponse response) {
+        if (!isContentLengthSet(response)) {
+            setContentLength(response, response.content().writerIndex());
+        }
+    }
+
+    private void prepareForKeepAlive(final FullHttpRequest request, final FullHttpResponse response, MocoHandler mocoHandler) {
+        if (isKeepAlive(request)) {
+            setKeepAlive(response, true);
+            setContentLengthForKeepAlive(response);
+        }
+    }
+}
--- moco-core/src/main/java/com/github/dreamhead/moco/internal/MocoHandler.java
+++ moco-core/src/main/java/com/github/dreamhead/moco/internal/MocoHandler.java
@@ -1,8 +1,5 @@
 package com.github.dreamhead.moco.internal;
 
-import com.github.dreamhead.moco.HttpRequest;
-import com.github.dreamhead.moco.model.DefaultHttpRequest;
-import com.github.dreamhead.moco.model.DefaultMutableHttpResponse;
 import com.github.dreamhead.moco.util.Strings;
 import io.netty.channel.ChannelFuture;
 import io.netty.channel.ChannelFutureListener;
@@ -15,22 +12,19 @@ import io.netty.handler.codec.http.HttpResponseStatus;
 import io.netty.handler.codec.http.HttpVersion;
 import io.netty.handler.codec.http.websocketx.WebSocketFrame;
 
-import static com.github.dreamhead.moco.model.DefaultMutableHttpResponse.newResponse;
 import static com.google.common.net.HttpHeaders.UPGRADE;
 import static io.netty.channel.ChannelHandler.Sharable;
-import static io.netty.handler.codec.http.HttpUtil.isContentLengthSet;
 import static io.netty.handler.codec.http.HttpUtil.isKeepAlive;
-import static io.netty.handler.codec.http.HttpUtil.setContentLength;
-import static io.netty.handler.codec.http.HttpUtil.setKeepAlive;
 
 @Sharable
 public final class MocoHandler extends SimpleChannelInboundHandler<Object> {
-    private static final int DEFAULT_STATUS = HttpResponseStatus.OK.code();
     private final ActualHttpServer server;
+    private final HttpHandler httpHandler;
     private final WebSocketHandler websocketHandler;
 
     public MocoHandler(final ActualHttpServer server) {
         this.server = server;
+        this.httpHandler = new HttpHandler(server);
         this.websocketHandler = new WebSocketHandler(server.getWebsocketServer());
     }
 
@@ -58,7 +52,7 @@ public final class MocoHandler extends SimpleChannelInboundHandler<Object> {
         }
 
         if (!upgradeWebsocket(request)) {
-            FullHttpResponse response = handleRequest(request);
+            FullHttpResponse response = httpHandler.handleRequest(request, this);
             closeIfNotKeepAlive(request, ctx.write(response));
             return;
         }
@@ -80,47 +74,9 @@ public final class MocoHandler extends SimpleChannelInboundHandler<Object> {
         ctx.flush();
     }
 
-    private FullHttpResponse handleRequest(final FullHttpRequest message) {
-        HttpRequest request = DefaultHttpRequest.newRequest(message);
-        DefaultMutableHttpResponse httpResponse = getHttpResponse(request);
-        FullHttpResponse response = httpResponse.toFullResponse();
-        prepareForKeepAlive(message, response);
-        return response;
-    }
-
-    private DefaultMutableHttpResponse getHttpResponse(final HttpRequest request) {
-        DefaultMutableHttpResponse httpResponse = newResponse(request, DEFAULT_STATUS);
-        SessionContext context = new SessionContext(request, httpResponse);
-        return doGetResponse(request, context);
-    }
-
-    private DefaultMutableHttpResponse doGetResponse(final HttpRequest request, final SessionContext context) {
-        try {
-            return (DefaultMutableHttpResponse) server.getResponse(context)
-                    .orElse(newResponse(request, HttpResponseStatus.BAD_REQUEST.code()));
-        } catch (RuntimeException e) {
-            return newResponse(request, HttpResponseStatus.BAD_REQUEST.code());
-        } catch (Exception e) {
-            return newResponse(request, HttpResponseStatus.INTERNAL_SERVER_ERROR.code());
-        }
-    }
-
     private void closeIfNotKeepAlive(final FullHttpRequest request, final ChannelFuture future) {
         if (!isKeepAlive(request)) {
             future.addListener(ChannelFutureListener.CLOSE);
-        }
-    }
-
-    private void prepareForKeepAlive(final FullHttpRequest request, final FullHttpResponse response) {
-        if (isKeepAlive(request)) {
-            setKeepAlive(response, true);
-            setContentLengthForKeepAlive(response);
-        }
-    }
-
-    private void setContentLengthForKeepAlive(final FullHttpResponse response) {
-        if (!isContentLengthSet(response)) {
-            setContentLength(response, response.content().writerIndex());
         }
     }
 
