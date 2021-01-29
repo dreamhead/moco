ab7f3004734d2149e688d1e69bea4b54acd85e02
dreamhead <dreamhead.cn@gmail.com>
2021-01-29 20:41:33 +0800

    initialized version directly in moco version

--- moco-core/src/main/java/com/github/dreamhead/moco/MocoVersion.java
+++ moco-core/src/main/java/com/github/dreamhead/moco/MocoVersion.java
@@ -1,11 +1,7 @@
 package com.github.dreamhead.moco;
 
 public final class MocoVersion {
-    public static final String VERSION;
-
-    static {
-        VERSION = MocoVersion.class.getPackage().getImplementationVersion();
-    }
+    public static final String VERSION = MocoVersion.class.getPackage().getImplementationVersion();
 
     private MocoVersion() {
     }
