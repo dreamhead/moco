aaa96449fb860d497de3542e7bb0ffb09ea3204c
dreamhead <dreamhead.cn@gmail.com>
2021-02-05 19:58:48 +0800

    added test for tail with null in iterables

--- moco-core/src/test/java/com/github/dreamhead/moco/util/IterablesTest.java
+++ moco-core/src/test/java/com/github/dreamhead/moco/util/IterablesTest.java
@@ -23,6 +23,7 @@ public class IterablesTest {
         assertThat(Iterables.tail(new Integer[]{1, 2}), is(new Integer[] {2}));
         assertThat(Iterables.tail(new Integer[1]), is(new Integer[0]));
         assertThat(Iterables.tail(new Integer[0]), is(new Integer[0]));
+        assertThrows(NullPointerException.class, () -> Iterables.tail(null));
     }
 
     @Test
