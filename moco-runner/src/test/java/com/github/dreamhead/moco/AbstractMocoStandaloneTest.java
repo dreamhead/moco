c1a9a45453ce4816092c00be402d2afc5b17f8be
dreamhead <dreamhead.cn@gmail.com>
2021-05-04 21:25:51 +0800

    applied stream to abstact moco standalone test

--- moco-runner/src/test/java/com/github/dreamhead/moco/AbstractMocoStandaloneTest.java
+++ moco-runner/src/test/java/com/github/dreamhead/moco/AbstractMocoStandaloneTest.java
@@ -8,10 +8,12 @@ import org.junit.After;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.Arrays;
 
 import static com.github.dreamhead.moco.bootstrap.arg.HttpArgs.httpArgs;
 import static com.github.dreamhead.moco.helper.RemoteTestUtils.port;
 import static com.github.dreamhead.moco.runner.JsonRunner.newJsonRunnerWithStreams;
+import static com.google.common.collect.ImmutableList.toImmutableList;
 
 public class AbstractMocoStandaloneTest {
     protected final MocoTestHelper helper = new MocoTestHelper();
@@ -25,19 +27,22 @@ public class AbstractMocoStandaloneTest {
     }
 
     protected void runWithConfiguration(final String... resourceNames) {
+        runner = newRunner(resourceNames);
+        runner.run();
+    }
+
+    private JsonRunner newRunner(final String[] resourceNames) {
+        final ImmutableList<InputStream> streams = Arrays.stream(resourceNames)
+                .map(this::resourceAsStream)
+                .collect(toImmutableList());
+        return newJsonRunnerWithStreams(streams, httpArgs().withPort(port()).build());
+    }
+
+    private InputStream resourceAsStream(String resourceName) {
         try {
-            runner = newRunner(resourceNames);
-            runner.run();
+            return Resources.getResource(resourceName).openStream();
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
-    }
-
-    private JsonRunner newRunner(final String[] resourceNames) throws IOException {
-        ImmutableList.Builder<InputStream> builder = ImmutableList.builder();
-        for (String resourceName : resourceNames) {
-            builder.add(Resources.getResource(resourceName).openStream());
-        }
-        return newJsonRunnerWithStreams(builder.build(), httpArgs().withPort(port()).build());
     }
 }
