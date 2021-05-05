b2d19f3067a04b5ad78dc1bae2c368647dc63066
dreamhead <dreamhead.cn@gmail.com>
2021-05-05 20:03:52 +0800

    applied stream http dumpers

--- moco-core/src/main/java/com/github/dreamhead/moco/dumper/HttpDumpers.java
+++ moco-core/src/main/java/com/github/dreamhead/moco/dumper/HttpDumpers.java
@@ -3,15 +3,17 @@ package com.github.dreamhead.moco.dumper;
 import com.github.dreamhead.moco.HttpMessage;
 import com.github.dreamhead.moco.model.MessageContent;
 import com.google.common.base.Joiner;
-import com.google.common.collect.ImmutableList;
 import com.google.common.collect.Maps;
 import com.google.common.net.HttpHeaders;
 import com.google.common.net.MediaType;
 import io.netty.util.internal.StringUtil;
 
+import java.util.Arrays;
 import java.util.Map;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
+
+import static com.google.common.collect.ImmutableList.toImmutableList;
 
 public final class HttpDumpers {
     public static String asContent(final HttpMessage message) {
@@ -65,11 +67,10 @@ public final class HttpDumpers {
 
     private static Stream<Map.Entry<String, String>> toEntries(final Map.Entry<String, String[]> input) {
         String key = input.getKey();
-        ImmutableList.Builder<Map.Entry<String, String>> builder = ImmutableList.builder();
-        for (String value : input.getValue()) {
-            builder.add(Maps.immutableEntry(key, value));
-        }
-        return builder.build().stream();
+        return Arrays.stream(input.getValue())
+                .map(value -> Maps.immutableEntry(key, value))
+                .collect(toImmutableList())
+                .stream();
     }
 
     private HttpDumpers() {
