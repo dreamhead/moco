bd7bfd066d1a8248e8916904350c11ec3a6b1518
dreamhead <dreamhead.cn@gmail.com>
2021-08-15 21:26:33 +0800

    applied optional to rest handler

--- moco-core/src/main/java/com/github/dreamhead/moco/rest/RestHandler.java
+++ moco-core/src/main/java/com/github/dreamhead/moco/rest/RestHandler.java
@@ -24,12 +24,9 @@ public final class RestHandler extends AbstractHttpResponseHandler {
     @Override
     protected void doWriteToResponse(final HttpRequest httpRequest, final MutableHttpResponse httpResponse) {
         Optional<ResponseHandler> responseHandler = dispatcher.getResponseHandler(httpRequest);
-        if (responseHandler.isPresent()) {
-            responseHandler.get().writeToResponse(new SessionContext(httpRequest, httpResponse));
-            return;
-        }
-
-        throw new UnsupportedOperationException("Unsupported REST request");
+        responseHandler
+                .orElseThrow(() -> new UnsupportedOperationException("Unsupported REST request"))
+                .writeToResponse(new SessionContext(httpRequest, httpResponse));
     }
 
     @Override
