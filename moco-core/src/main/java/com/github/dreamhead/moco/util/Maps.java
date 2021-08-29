733fa5da9cbcaf922953e8a8d9354cc0f6901ef7
dreamhead <dreamhead.cn@gmail.com>
2021-08-29 20:19:09 +0800

    applied stream to iterable value to array in maps

--- moco-core/src/main/java/com/github/dreamhead/moco/util/Maps.java
+++ moco-core/src/main/java/com/github/dreamhead/moco/util/Maps.java
@@ -1,6 +1,5 @@
 package com.github.dreamhead.moco.util;
 
-import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.Iterables;
 
 import java.util.Map;
@@ -21,13 +20,9 @@ public final class Maps {
     }
 
     public static Map<String, String[]> iterableValueToArray(final Map<String, Iterable<String>> map) {
-        ImmutableMap.Builder<String, String[]> builder = ImmutableMap.builder();
-        for (Map.Entry<String, Iterable<String>> entry : map.entrySet()) {
-            Iterable<String> value = entry.getValue();
-            builder.put(entry.getKey(), Iterables.toArray(value, String.class));
-        }
-
-        return builder.build();
+        return map.entrySet()
+                .stream()
+                .collect(toImmutableMap(Map.Entry::getKey, e -> Iterables.toArray(e.getValue(), String.class)));
     }
 
     private Maps() {
