1934a1638da180b04d8be1c3d72ec366cd820040
dreamhead <dreamhead.cn@gmail.com>
2022-08-24 19:09:59 +0800

    replaced content type in http client with guava

--- moco-core/src/main/java/com/github/dreamhead/moco/recorder/InMemoryRequestRecorder.java
+++ moco-core/src/main/java/com/github/dreamhead/moco/recorder/InMemoryRequestRecorder.java
@@ -2,8 +2,8 @@ package com.github.dreamhead.moco.recorder;
 
 import com.github.dreamhead.moco.HttpRequest;
 import com.github.dreamhead.moco.model.MessageContent;
+import com.google.common.net.HttpHeaders;
 import com.google.common.net.MediaType;
-import org.apache.http.HttpHeaders;
 
 public class InMemoryRequestRecorder implements RequestRecorder {
     private HttpRequest httpRequest;
