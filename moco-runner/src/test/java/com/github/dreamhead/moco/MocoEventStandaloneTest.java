d914b16b95f6efb00620341a8278cd809bc1215d
dreamhead <dreamhead.cn@gmail.com>
2022-08-29 20:40:21 +0800

    fixed test failure in event stanalone test

--- moco-runner/src/test/java/com/github/dreamhead/moco/MocoEventStandaloneTest.java
+++ moco-runner/src/test/java/com/github/dreamhead/moco/MocoEventStandaloneTest.java
@@ -19,7 +19,7 @@ import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class MocoEventStandaloneTest extends AbstractMocoStandaloneTest {
-    private static final long IDLE = 1200;
+    private static final long IDLE = 1500;
 
     @Rule
     public TemporaryFolder folder = new TemporaryFolder();
