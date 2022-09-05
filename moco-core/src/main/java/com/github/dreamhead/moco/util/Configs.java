46aea55573eff16f9f0f01ebd0f061086512bce1
dreamhead <dreamhead.cn@gmail.com>
2022-09-05 21:55:36 +0800

    added generic in configs

--- moco-core/src/main/java/com/github/dreamhead/moco/util/Configs.java
+++ moco-core/src/main/java/com/github/dreamhead/moco/util/Configs.java
@@ -9,20 +9,20 @@ import java.util.List;
 import static com.google.common.base.Preconditions.checkNotNull;
 
 public final class Configs {
-    public static <T extends ConfigApplier<T>> T configItem(final T source, final MocoConfig... configs) {
+    public static <T extends ConfigApplier<T>> T configItem(final T source, final MocoConfig<?>... configs) {
         if (source == null) {
             return null;
         }
 
         T target = source;
-        for (MocoConfig config : configs) {
+        for (MocoConfig<?> config : configs) {
             target = target.apply(config);
         }
         return target;
     }
 
     public static <T extends ConfigApplier<T>> ImmutableList<T> configItems(final List<T> items,
-                                                                            final MocoConfig... configs) {
+                                                                            final MocoConfig<?>... configs) {
         checkNotNull(items, "config items should not be null");
         return items.stream()
                 .map(item -> configItem(item, configs))
