2b38debede7c57b3d854819888c8de5167edb6bb
dreamhead <dreamhead.cn@gmail.com>
2023-07-28 19:21:22 +0800

    upgraded netty to 4.1.95

--- moco-core/src/test/java/com/github/dreamhead/moco/MocoTemplateTest.java
+++ moco-core/src/test/java/com/github/dreamhead/moco/MocoTemplateTest.java
@@ -379,7 +379,7 @@ public class MocoTemplateTest extends AbstractMocoHttpTest {
             String response = helper.get(remoteUrl("/random"));
             try {
                 double result = Double.parseDouble(response);
-                assertThat(result, lessThan(100d));
+                assertThat(result, lessThanOrEqualTo(100d));
                 assertThat(result, greaterThanOrEqualTo(99d));
             } catch (NumberFormatException e) {
                 fail();
