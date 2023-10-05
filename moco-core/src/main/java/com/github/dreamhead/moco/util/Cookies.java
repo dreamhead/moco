1d4db0563c83c6ccccedb98eb48607328d3cd9f4
dreamhead <dreamhead.cn@gmail.com>
2023-10-05 22:41:17 +0800

    removed unused imports in cookies

--- moco-core/src/main/java/com/github/dreamhead/moco/util/Cookies.java
+++ moco-core/src/main/java/com/github/dreamhead/moco/util/Cookies.java
@@ -2,9 +2,8 @@ package com.github.dreamhead.moco.util;
 
 import com.github.dreamhead.moco.CookieAttribute;
 import com.github.dreamhead.moco.cookie.ActualCookieAttribute;
-import io.netty.handler.codec.http.cookie.ServerCookieEncoder;
-import io.netty.handler.codec.http.cookie.Cookie;
 import io.netty.handler.codec.http.cookie.DefaultCookie;
+import io.netty.handler.codec.http.cookie.ServerCookieEncoder;
 
 public final class Cookies {
     public String encodeCookie(final String key, final String value, final CookieAttribute... options) {
