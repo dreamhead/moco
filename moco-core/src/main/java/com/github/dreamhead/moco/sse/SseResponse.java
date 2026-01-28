90220b1f9a49243bf356ca0ceb06a6adadcc2718
Zheng Ye <dreamhead.cn@gmail.com>
2026-01-28 22:58:29 +0800

    ensured event and data in sse contain at least 1 data argument

--- moco-core/src/main/java/com/github/dreamhead/moco/sse/SseResponse.java
+++ moco-core/src/main/java/com/github/dreamhead/moco/sse/SseResponse.java
@@ -1,11 +1,17 @@
 package com.github.dreamhead.moco.sse;
 
+import com.github.dreamhead.moco.util.Iterables;
+
+import java.util.List;
+
 public class SseResponse {
-    public SseResponseBuilder event(final String eventName, final String... data) {
-        return new SseResponseBuilder().event(eventName, data);
+    public SseResponseBuilder event(final String eventName, final String first, final String... rest) {
+        List<String> data = Iterables.asIterable(first, rest);
+        return new SseResponseBuilder().event(eventName, data.toArray(new String[0]));
     }
 
-    public SseResponseBuilder data(final String... data) {
-        return new SseResponseBuilder().data(data);
+    public SseResponseBuilder data(final String first, final String... rest) {
+        List<String> data = Iterables.asIterable(first, rest);
+        return new SseResponseBuilder().data(data.toArray(new String[0]));
     }
 }
